6ec6bb07f5979a1c3ea115f982f537bd497194ab
Fahad Hussain <sfahadh14@gmail.com>
2019-11-12 19:49:09 -0500

    added Letter Case Perutation problem

---
+++ Letter-Case-Permutation/problem.js
@@ -0,0 +1,11 @@
+// Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.  Return a list of all possible strings we could create.
+
+// Link: https://leetcode.com/problems/letter-case-permutation/
+
+const letterCasePermutation = S => {
+
+};
+
+// Examples:
+// Input: S = "a1b2"
+// Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
