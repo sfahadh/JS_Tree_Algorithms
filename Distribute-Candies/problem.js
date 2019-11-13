978e045a501d67f23d35bc970851bd921372dbba
Fahad Hussain <sfahadh14@gmail.com>
2019-11-12 19:12:58 -0500

    added and modified test cases

--- Distribute-Candies/problem.js
+++ Distribute-Candies/problem.js
@@ -3,10 +3,14 @@
 // Link: https://leetcode.com/problems/distribute-candies/
 
 const distributeCandies = candies => {
+    const hashmap = {}
+    for (let candy of candies) {
+        hashmap[candy] ? hashmap[candy]++ : hashmap[candy] = 1
+    }
 
+    return Math.min(Object.keys(hashmap).length, candies.length / 2)
 };
 
-console.log(distributeCandies([1, 1, 2, 2, 3, 3]))
 // Output: 3
 /* There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
 Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too. 
--- Distribute-Candies/problem.test.js
+++ Distribute-Candies/problem.test.js
@@ -1,6 +1,11 @@
 const distributeCandies = require('./problem')
 
-test('returns maximum number of candies sister can gain', () => {
+test('returns maximum number of candies sister can gain of 3', () => {
     const arr = [1, 1, 2, 2, 3, 3]
-    expect(distributeCandies(arr)).toBeTruthy()
+    expect(distributeCandies(arr)).toEqual(3)
+})
+
+test('returns maximum number of candies sister can gain of 2', () => {
+    const arr = [1, 1, 2, 3]
+    expect(distributeCandies(arr)).toEqual(2)
 })
