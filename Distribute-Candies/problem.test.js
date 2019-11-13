bc35c7f362f159f4f81a5c3bc2d0b636887a36bb
Fahad Hussain <sfahadh14@gmail.com>
2019-11-12 19:05:23 -0500

    added test file for distributed candies

---
+++ Distribute-Candies/problem.test.js
@@ -0,0 +1,6 @@
+const distributeCandies = require('./problem')
+
+test('returns maximum number of candies sister can gain', () => {
+    const arr = [1, 1, 2, 2, 3, 3]
+    expect(distributeCandies(arr).toBeTruthy())
+})
