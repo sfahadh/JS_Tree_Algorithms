219c01194173dc4f8585b62eef178595c7b7f1a2
Fahad Hussain <sfahadh14@gmail.com>
2019-11-17 16:42:08 -0500

    added Unique Character Mapping problem

---
+++ Unique-Character-Mapping/problem.js
@@ -0,0 +1,17 @@
+// Write a function that returns a character mapping from a word.
+
+// Link: https://edabit.com/challenge/9MEWMJ5eX8EAiWSs5
+
+const characterMapping = str => {
+
+}
+
+// Examples:
+// characterMapping("abcd") ➞ [0, 1, 2, 3]
+// characterMapping("abb") ➞ [0, 1, 1]
+// characterMapping("babbcb") ➞ [0, 1, 0, 0, 2, 0]
+// characterMapping("hmmmmm") ➞ [0, 1, 1, 1, 1, 1]
+
+// Notes:
+// Start your counter at 0, and increment by 1 each time you encounter a new character.
+// Identical characters should share the same number.
