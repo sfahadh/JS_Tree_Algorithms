e9b43c66952073e39db1e76a9080aaff3e5587a2
Fahad Hussain <sfahadh14@gmail.com>
2019-11-17 00:34:33 -0500

    removed solution in the problem file of double palindromes

--- Double-Palindromes/problem.js
+++ Double-Palindromes/problem.js
@@ -6,38 +6,7 @@
 // Link: https://edabit.com/challenge/KsbT9c2zf7jczanTt
 
 const palindromeSet = arr => {
-    let numString = ''
-    let letterString = ''
-    let counter = 0
 
-    const letters = /^[A-Za-z]*$/
-    const numbers = /^[0-9]*$/
-    const results = []
-
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            if (arr[i][j].match(letters)) {
-                letterString += arr[i][j]
-            } else if (arr[i][j].match(numbers)) {
-                numString += arr[i][j]
-            }
-        }
-
-        if (letterString === letterString.split('').reverse().join('') && letterString) {
-            counter++
-        }
-        letterString = ''
-
-        if (numString === numString.split('').reverse().join('') && numString) {
-            counter++
-        }
-        numString = ''
-
-        results.push(counter)
-        counter = 0
-    }
-
-    return results
 }
 
 // "cab97ac79" // double-palindrome
