486657434f23a71e47bb5e995258d55d57648596
Fahad Hussain <sfahadh14@gmail.com>
2019-11-28 17:03:42 -0500

    added Depth of Array problem

---
+++ Depth-of-Array/problem.js
@@ -0,0 +1,16 @@
+// Given an array, write a function to calculate it's depth. Assume that a normal array has a depth of 1.
+
+// https://edabit.com/challenge/JJtafXGmNegpQMp6p
+
+const depth = arr => {
+
+}
+
+// Examples
+// depth([1, 2, 3, 4]) ➞ 1
+
+// depth([1, [2, 3, 4]]) ➞ 2
+
+// depth([1, [2, [3, 4]]]) ➞ 3
+
+// depth([1, [2, [3, [4]]]]) ➞ 4
