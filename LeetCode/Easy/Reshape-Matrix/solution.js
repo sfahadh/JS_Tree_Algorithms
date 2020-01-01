14b802b6bda93873050ee9e727ff86ab095122d5
Fahad Hussain <sfahadh14@gmail.com>
2020-01-01 11:56:11 -0500

    installed node modules

--- LeetCode/Easy/Reshape-Matrix/solution.js
+++ LeetCode/Easy/Reshape-Matrix/solution.js
@@ -1,28 +1,8 @@
 const matrixReshape = (nums, r, c) => {
-    const flat = nums.flat()
-    if (flat.length > r * c) {
-        return nums
-    }
 
-    if (r === 1) {
-        return nums.flat()
-    }
-
-    const result = []
-    let counter = r
-    for (let i = 0; i < flat.length; i++) {
-        if (counter === r) {
-            result.push([])
-            counter = 0
-        } else {
-            counter++
-            result[result.length - 1].push(flat[i])
-        }
-    }
-
-    return result
 }
 
+// Example test case:
 console.log(matrixReshape([
     [1, 2, 6],
     [3, 4, 1],
