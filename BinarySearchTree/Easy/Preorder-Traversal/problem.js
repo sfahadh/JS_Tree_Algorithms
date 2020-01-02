7722247a7c39c9b81746713146b6d430365aedf2
Fahad Hussain <sfahadh14@gmail.com>
2020-01-02 16:51:38 -0500

    removed HackerRank folder and added BinarySearchTree folder

---
+++ BinarySearchTree/Easy/Preorder-Traversal/problem.js
@@ -0,0 +1,40 @@
+/*
+Complete the preOrder function in your editor below, which has  parameter: a pointer to the root of a binary tree. It must print the values in the tree's preorder traversal as a single line of space-separated values.
+
+Link: https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+*/
+
+function Node(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null; 
+}
+
+function BST() {
+    this.root = null;
+}
+
+Node.prototype.preOrder = function() {
+    console.log(this.value + " ");
+    if (root.left) preOrder(root.left);
+    if (root.right) preOrder(root.right);
+}
+
+const node1 = new Node(5);
+node1.preOrder();
+
+/*
+Sample Input: 
+     1
+      \
+       2
+        \
+         5
+        /  \
+       3    6
+        \
+         4  
+
+Sample Output:
+1 2 5 3 4 6 
+*/
