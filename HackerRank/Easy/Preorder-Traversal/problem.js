d96f520c99f0e16369f0fbe9cfa0382d5b5eb899
Fahad Hussain <sfahadh14@gmail.com>
2020-01-02 00:43:10 -0500

    added problem, problem.test, and solution file for preorder

---
+++ HackerRank/Easy/Preorder-Traversal/problem.js
@@ -0,0 +1,23 @@
+/*
+Complete the preOrder function in your editor below, which has  parameter: a pointer to the root of a binary tree. It must print the values in the tree's preorder traversal as a single line of space-separated values.
+
+Sample Input: 
+     1
+      \
+       2
+        \
+         5
+        /  \
+       3    6
+        \
+         4  
+
+Sample Output:
+1 2 5 3 4 6 
+
+Link: https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+*/
+
+const preOrder = root => {
+    
+}
