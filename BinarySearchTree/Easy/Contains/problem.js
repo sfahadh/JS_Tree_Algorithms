a9cbd0fdf84bcd4c0e35af0d393feb2f2ae3b68c
Fahad Hussain <sfahadh14@gmail.com>
2020-01-03 16:58:16 -0500

    added all test cases for contains problem

--- BinarySearchTree/Easy/Contains/problem.js
+++ BinarySearchTree/Easy/Contains/problem.js
@@ -9,11 +9,7 @@ function Node(data) {
 */
 
 const contains = (tree, node) => {
-    if (!tree) return false;
-    if (node === tree.data) return true;
-    if (node < tree.data) return contains(tree.left, node);
-    if (node > tree.data) return contains(tree.right, node);
-    return false;
+
 }
 
 module.exports = contains;
--- BinarySearchTree/Easy/Contains/problem.test.js
+++ BinarySearchTree/Easy/Contains/problem.test.js
@@ -1,7 +1,7 @@
 const Node = require("../../BST/starter");
 const contains = require("./problem");
 
-test("Return true", () => {
+test("Node is deep in the tree, return true", () => {
     const tree = new Node(7);
     tree.insert(5);
     tree.insert(13);
@@ -14,8 +14,8 @@ test("Return true", () => {
 });
 /*
 
-  		  7
-  	    /	\
+  		    7
+  	    /	  \
        5     13
      /  \   /  \
     2    6 9   15
@@ -24,7 +24,7 @@ test("Return true", () => {
 
 */
 
-test("Return true", () => {
+test("Node is in the root of tree, return true", () => {
     const tree = new Node(43);
     tree.insert(25);
     tree.insert(65);
@@ -37,3 +37,44 @@ test("Return true", () => {
    25      65
 
 */
+
+test("Return true", () => {
+    const tree = new Node(15);
+    tree.insert(16);
+    tree.insert(3);
+    tree.insert(21);
+    expect(contains(tree, 15)).toEqual(true);
+});
+/*
+
+  	   15
+  	 /	  \
+    3      16
+             \
+              21
+
+*/
+
+test("Node is not in tree, return false", () => {
+    const tree = new Node(15);
+    tree.insert(17);
+    tree.insert(3);
+    tree.insert(21);
+    tree.insert(8);
+    tree.insert(2);
+    tree.insert(16);
+    expect(contains(tree, 12)).toEqual(false);
+});
+/*
+
+  	   15
+  	 /	  \
+    3      17
+  /  \    /  \
+ 2    8  16   21
+
+*/
+
+test("No nodes in empty tree, return false", () => {
+  expect(contains(null, 10)).toEqual(false);
+});
