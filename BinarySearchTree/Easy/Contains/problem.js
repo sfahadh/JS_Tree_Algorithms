9dfc5af51c82456c59c1e4688b270bb8c41ac369
Fahad Hussain <sfahadh14@gmail.com>
2020-01-03 16:18:17 -0500

    added contains problem

---
+++ BinarySearchTree/Easy/Contains/problem.js
@@ -0,0 +1,21 @@
+/*
+Complete the following BST contains problem below. You must check if the tree contains the input node. The return value must be either true or false.
+*/
+
+const contains = node => {
+
+}
+
+/*
+
+  		  7
+  	    /	\
+       5     13
+     /  \   /  \
+    2    6 9   15
+   /
+  1 
+
+contains(6) -> return true  
+contains(12) -> return false
+*/
