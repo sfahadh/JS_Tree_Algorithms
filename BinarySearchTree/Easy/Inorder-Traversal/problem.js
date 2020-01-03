1181817f857a94a074ccf8c217b32c7f442c9029
Fahad Hussain <sfahadh14@gmail.com>
2020-01-02 23:48:21 -0500

    added problem file for in order

---
+++ BinarySearchTree/Easy/Inorder-Traversal/problem.js
@@ -0,0 +1,23 @@
+/*
+Complete the following in-order BST traversal problem below. The input is the whole tree structure starting from the root. The returned result must be an array.
+*/
+
+const inOrder = root => {
+
+}
+
+module.exports = inOrder;
+
+/*
+Sample Input: 
+     24
+    /  \
+   17  31    
+         \ 
+         34
+        /  \
+       32  39
+
+Sample Output:
+[24, 17, 31, 34, 32, 39] 
+*/
