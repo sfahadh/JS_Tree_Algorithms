8a5b4e609303f7b97b8c3d2659c94a3da94318a3
Fahad Hussain <sfahadh14@gmail.com>
2020-01-02 23:40:59 -0500

    added solution file to post order tree problem

---
+++ BinarySearchTree/Easy/Postorder-Traversal/solution.js
@@ -0,0 +1,11 @@
+const postOrder = root => {
+    const array = [];
+    const recurse = root => {
+        if (root === null) return [];
+        if (root.left) recurse(root.left);
+        if (root.right) recurse(root.right);
+        array.push(root.data)
+    }
+    recurse(root);
+    return array;
+}
