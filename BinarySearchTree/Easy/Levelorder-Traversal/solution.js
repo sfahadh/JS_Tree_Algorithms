25ff08342c25eba38f7daf7fa4910b0db13e3a36
Fahad Hussain <sfahadh14@gmail.com>
2020-01-03 00:41:16 -0500

    added solution file for level order problem

---
+++ BinarySearchTree/Easy/Levelorder-Traversal/solution.js
@@ -0,0 +1,12 @@
+const levelOrder = root => {
+    if (root === null) return [];
+    const array = [];
+    const queue = [root];
+    while (queue.length) {
+          const branch = queue.shift();
+          array.push(branch.data);
+          if (branch.left) queue.push(branch.left);
+          if (branch.right) queue.push(branch.right);
+    }
+    return array;
+}
