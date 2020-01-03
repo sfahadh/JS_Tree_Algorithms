1a2c0de45124a7ccfb79e3c50529e908123efd08
Fahad Hussain <sfahadh14@gmail.com>
2020-01-03 00:40:35 -0500

    added test file for level order traversal problem

--- BinarySearchTree/Easy/Levelorder-Traversal/problem.js
+++ BinarySearchTree/Easy/Levelorder-Traversal/problem.js
@@ -3,7 +3,16 @@ Complete the following level-order BST traversal problem below. The input is the
 */
 
 const levelOrder = root => {
-
+      if (root === null) return [];
+      const array = [];
+      const queue = [root];
+      while (queue.length) {
+            const branch = queue.shift();
+            array.push(branch.data);
+            if (branch.left) queue.push(branch.left);
+            if (branch.right) queue.push(branch.right);
+      }
+      return array;
 }
 
 module.exports = levelOrder;
---
+++ BinarySearchTree/Easy/Levelorder-Traversal/problem.test.js
@@ -0,0 +1,42 @@
+const Node = require("../../BST/starter");
+const levelOrder = require("./problem");
+
+test("Return an array of values - [10, 6, 15]", () => {
+    const node = new Node(10);
+    node.insert(6);
+    node.insert(15);
+    expect(levelOrder(node)).toEqual([10, 6, 15]);
+});
+
+test("Return an array of value - [33]", () => {
+    const node = new Node(33);
+    expect(levelOrder(node)).toEqual([33]);
+});
+
+test("Return an array of values - [24, 17, 31, 34, 32, 39]", () => {
+    const node = new Node(24);
+    node.insert(24);
+    node.insert(17);
+    node.insert(31);
+    node.insert(34);
+    node.insert(32);
+    node.insert(39);
+    expect(levelOrder(node)).toEqual([24, 17, 31, 34, 32, 39]);
+});
+
+test("Return an empty array", () => {
+    expect(levelOrder(null)).toEqual([]);
+});
+
+test("Return an array of values - [18, 5, 65, 3, 13, 33, 0]", () => {
+    const node = new Node(18);
+    node.insert(5);
+    node.insert(13);
+    node.insert(3);
+    node.insert(18);
+    node.insert(65);
+    node.insert(0);
+    node.insert(3);
+    node.insert(33);
+    expect(levelOrder(node)).toEqual([18, 5, 65, 3, 13, 33, 0]);
+});
