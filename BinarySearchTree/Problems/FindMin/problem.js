5e946e488d4ccf99c62ec2ac80a30a06b69b5d9d
Fahad Hussain <sfahadh14@gmail.com>
2020-01-04 10:52:39 -0500

    added problem file for Find Min

---
+++ BinarySearchTree/Problems/FindMin/problem.js
@@ -0,0 +1,26 @@
+/*
+Complete the following BST find min node problem below. You must traverse down the tree and find the node with the lowest value.
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const findMin = root => {
+
+}
+
+module.exports = findMin;
+
+/*
+
+	    25                          
+	   /  \             	   
+     16    26     
+    /  \     \           
+   5   21     35             
+
+Max node = 5
+*/
