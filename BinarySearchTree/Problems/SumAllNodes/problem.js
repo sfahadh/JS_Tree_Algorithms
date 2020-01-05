e7a6c52b7da826dd3ea50171ce99daf39d1c4e18
Fahad Hussain <sfahadh14@gmail.com>
2020-01-05 13:48:53 -0500

    added sum all nodes problem

---
+++ BinarySearchTree/Problems/SumAllNodes/problem.js
@@ -0,0 +1,26 @@
+/*
+Complete the following BST sum all nodes problem below. There is only 1 input and the goal is to add up the values of all nodes and return the total.
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const sum = root => {
+
+}
+
+module.exports = sum;
+
+/*
+
+	    25                   
+	   /  \             	   
+     16    26      
+    /  \     \           
+   5   21     35         
+
+16 + 21 + 25 + 26 + 5 + 35 = 128
+*/
