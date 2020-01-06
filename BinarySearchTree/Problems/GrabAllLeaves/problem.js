01a52d290d611b6323425180a54a41121696fd98
Fahad Hussain <sfahadh14@gmail.com>
2020-01-06 13:53:20 -0500

    added grab all leaves problem

---
+++ BinarySearchTree/Problems/GrabAllLeaves/problem.js
@@ -0,0 +1,24 @@
+/*
+Complete the following BST grab all leaves problem below. There is only 1 input and the goal is to return all the leaf nodes from the BST.
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const grabAllLeaves = root => {
+}
+
+module.exports = grabAllLeaves;
+
+/*
+
+	    25                   
+	   /  \             	   
+     16    26      
+    /  \     \           
+   5   21     35         
+
+Output: [5, 21, 35]
+*/
