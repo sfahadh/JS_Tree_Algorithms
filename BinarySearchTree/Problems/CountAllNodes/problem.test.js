f393ae4bb1e10e097dc88a908aaa55c6fca73a15
Fahad Hussain <sfahadh14@gmail.com>
2020-01-06 21:37:37 -0500

    added test cases for count all nodes problem

--- BinarySearchTree/Problems/CountAllNodes/problem.test.js
+++ BinarySearchTree/Problems/CountAllNodes/problem.test.js
@@ -1,7 +1,7 @@
 const Node = require("../../BST/starter");
-const sum = require("./problem");
+const countNodes = require("./problem");
 
-test("The sum of all nodes should return 58", () => {
+test("Return 8 for number of nodes in BST", () => {
     const tree = new Node(7);
     tree.insert(5);
     tree.insert(13);
@@ -10,11 +10,11 @@ test("The sum of all nodes should return 58", () => {
     tree.insert(9);
     tree.insert(15);
     tree.insert(1);
-    expect(sum(tree)).toEqual(58);
+    expect(countNodes(tree)).toEqual(8);
 });
 /*
 
-  		    7
+  		  7
   	    /  	\
        5     13
      /  \   /  \
@@ -24,11 +24,11 @@ test("The sum of all nodes should return 58", () => {
 
 */
 
-test("The sum of all nodes should return 133", () => {
+test("Return 3 for number of nodes in BST", () => {
     const tree = new Node(43);
     tree.insert(25);
     tree.insert(65);
-    expect(sum(tree)).toEqual(133);
+    expect(countNodes(tree)).toEqual(3);
 });
 /*
 
@@ -38,9 +38,9 @@ test("The sum of all nodes should return 133", () => {
 
 */
 
-test("The sum of all nodes should return 17", () => {
+test("Return 1 for number of nodes in BST", () => {
     const tree = new Node(17);
-    expect(sum(tree)).toEqual(17);
+    expect(countNodes(tree)).toEqual(1);
 });
 /*
 
@@ -48,7 +48,7 @@ test("The sum of all nodes should return 17", () => {
 
 */
 
-test("The sum of all nodes should return 82", () => {
+test("Return 7 for number of nodes in BST", () => {
     const tree = new Node(15);
     tree.insert(17);
     tree.insert(3);
@@ -56,7 +56,7 @@ test("The sum of all nodes should return 82", () => {
     tree.insert(8);
     tree.insert(2);
     tree.insert(16);
-    expect(sum(tree)).toEqual(82);
+    expect(countNodes(tree)).toEqual(7);
 });
 /*
 
@@ -68,11 +68,11 @@ test("The sum of all nodes should return 82", () => {
 
 */
 
-test("The sum of all nodes should return 0", () => {
-  expect(sum(null)).toEqual(0);
+test("Return 0 because there is no tree", () => {
+  expect(countNodes(null)).toEqual(0);
 });
 
-test("The sum of all nodes should return 289", () => {
+test("Return 9 for number of nodes in BST", () => {
     const tree = new Node(25);
     tree.insert(16);
     tree.insert(26);
@@ -82,12 +82,12 @@ test("The sum of all nodes should return 289", () => {
     tree.insert(45);
     tree.insert(47);
     tree.insert(69);
-    expect(sum(tree)).toEqual(289);
+    expect(countNodes(tree)).toEqual(9);
 });
 /*
 
-	    25                          
-	   /  \             	   
+	   25                          
+	  /  \             	   
     16    26     
    /  \     \           
   5   21     35  
