bf8c772a3409e300fe9698a9b16d6111e351b7cd
Fahad Hussain <sfahadh14@gmail.com>
2020-01-06 20:35:19 -0500

    added remove all leaves problem

--- BinarySearchTree/Problems/RemoveALeaf/problem.js
+++ BinarySearchTree/Problems/RemoveALeaf/problem.js
@@ -1,5 +1,5 @@
 /*
-Complete the following BST remove a leaf problem below. There are 2 inputs, the tree and the leaf node that needs to be removed from the    tree. A leaf node is a node with no children. If the leaf node is found, return the tree without that leaf node, otherwise if the leaf node is not found, return original tree.
+Complete the following BST remove a leaf problem below. There are 2 inputs, the tree and the leaf node that needs to be removed from the  tree. A leaf node is a node with no children. If the leaf node is found, return the tree without that leaf node, otherwise if the leaf node is not found, return original tree.
 
 function Node(data) {
     this.data = data;
---
+++ BinarySearchTree/Problems/RemoveAllLeaves/problem.js
@@ -0,0 +1,25 @@
+/*
+Complete the following BST remove all leaves problem below. There is only one input, which is the BST. A leaf node is a node with no children. Remove all leaves found in the BST.
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const removeAllLeaves = root => {
+   
+}
+
+module.exports = removeAllLeaves;
+
+/*
+
+	    25                   25        
+	   /  \             	/  \      
+     16    26      =>     16    26    
+    /  \     \           
+   5   21     35         
+
+*/
