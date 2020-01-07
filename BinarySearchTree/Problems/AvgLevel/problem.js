f0c630b18b7e01e52dc4a78b1ad39d298fe4baa0
Fahad Hussain <sfahadh14@gmail.com>
2020-01-07 11:57:27 -0500

    added average levels problem

---
+++ BinarySearchTree/Problems/AvgLevel/problem.js
@@ -0,0 +1,29 @@
+/*
+Complete the following BST average level problem below. There is only one input, the BST. The goal is to return an array of the average of each levels of the BST. 
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const rangeSum = root => {
+
+}
+
+module.exports = rangeSum;
+
+/*
+
+	    25                   
+	   /  \             	   
+     16    26      
+    /  \     \           
+   5   21     34         
+
+Output: [25, 21, 20]
+25
+(26 + 16) / 2 = 21
+(5 + 26 + 34) / 3 = 21
+*/
