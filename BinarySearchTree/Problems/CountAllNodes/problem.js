e96611a0b0feebfd4da1c9892f83eb52c7cfa5d9
Fahad Hussain <sfahadh14@gmail.com>
2020-01-06 21:31:43 -0500

    added count all nodes problem

---
+++ BinarySearchTree/Problems/CountAllNodes/problem.js
@@ -0,0 +1,25 @@
+/*
+Complete the following BST count all nodes problem below. There is only 1 input and the goal is to return the total number of nodes found in the BST.
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const countNodes = root => {
+}
+
+module.exports = countNodes;
+
+/*
+
+	    25                   
+	   /  \             	   
+     16    26      
+    /  \     \           
+   5   21     35         
+
+Output: 6
+*/
