a0323442616b0de799e010ed90988ad31be86ce0
Fahad Hussain <sfahadh14@gmail.com>
2020-01-09 19:50:16 -0500

    added two sum problem

---
+++ BinarySearchTree/Problems/TwoSum/problem.js
@@ -0,0 +1,29 @@
+/*
+Complete the following BST two sum problem below. There are 2 inputs, the root of the BST, and the value in which 2 nodes should add up to. If the two nodes in the BST adds up to the input value, then return true, otherwise return false.
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const twoSum = (root, sum) => {
+}
+
+module.exports = twoSum;
+
+/*
+
+	    25                   
+	   /  \             	   
+     16    26      
+    /  \     \           
+   5   21     35         
+
+Input: (root, 37)
+Output: true
+
+Input: (root, 38)
+Output: false
+*/
