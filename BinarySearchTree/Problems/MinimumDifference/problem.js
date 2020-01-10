017e145b9fa2ed75b0d5d70430ef890cd5783bde
Fahad Hussain <sfahadh14@gmail.com>
2020-01-09 21:30:44 -0500

    added minimum difference problem

---
+++ BinarySearchTree/Problems/MinimumDifference/problem.js
@@ -0,0 +1,28 @@
+/*
+Complete the following BST minimum difference problem below. There is only one input, which is the root of the BST. The goal is to find the lowest output from subtracting 2 nodes.  
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const minimumDifference = root => {
+
+}
+
+module.exports = minimumDifference;
+
+/*
+
+	    25                   
+	   /  \             	   
+     16    26             
+
+26 - 25 = 1
+25 - 16 = 9
+26 - 16 = 10
+
+Output: 1   
+*/
