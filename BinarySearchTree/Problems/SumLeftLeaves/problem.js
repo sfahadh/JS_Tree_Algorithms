584e6460698ccfb7b5cc2915e14a5abaaad2a86e
Fahad Hussain <sfahadh14@gmail.com>
2020-01-10 22:27:51 -0500

    added sum left leaves problem

---
+++ BinarySearchTree/Problems/SumLeftLeaves/problem.js
@@ -0,0 +1,25 @@
+/*
+Complete the following BST sum left leaves problem below. There is only 1 input, which is the root of the BST and the goal is to add up all the left leaves and return the total.
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const sumLeftLeaves = root => {
+}
+
+module.exports = sumLeftLeaves;
+
+/*
+
+	     25                   
+	   /    \             	   
+     16      26      
+    /  \    /  \           
+   5   21  24   35      
+
+5 + 24 = 29
+*/
