6c833dd568252174d9dec450512f0c196c94878c
Fahad Hussain <sfahadh14@gmail.com>
2020-01-21 21:48:49 -0500

    added dual parent problem

---
+++ BinarySearchTree/Problems/DualParents/problem.js
@@ -0,0 +1,27 @@
+/*
+Complete the following BST dual parents problem below. There is only 1 input, which is the root of the BST. The goal of this problem is to return the number of parents that have both a left and right child node.
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const dualParent = root => {
+
+}
+
+module.exports = dualParent;
+
+/*
+
+	    25                          
+	   /  \             	   
+     16    26     
+    /  \     \           
+   5   21     35             
+
+Output: 2
+Both 25 and 16 have 2 child nodes
+*/
