6fb0df545d7969f2ddd82ac127010a2b4559314a
Fahad Hussain <sfahadh14@gmail.com>
2020-05-03 18:44:15 -0400

    fixed and added more test cases for countAllNodes level problem in BinarySearchTree

--- BinarySearchTree/Problems/CountAllNodes/problem.test.js
+++ BinarySearchTree/Problems/CountAllNodes/problem.test.js
@@ -1,16 +1,18 @@
-const Node = require("../../BST/starter");
+const BST = require("../../Starter/BST");
 const countNodes = require("./problem");
 
+// Test Case 1:
 test("Return 8 for number of nodes in BST", () => {
-    const tree = new Node(7);
-    tree.insert(5);
-    tree.insert(13);
-    tree.insert(2);
-    tree.insert(6);
-    tree.insert(9);
-    tree.insert(15);
-    tree.insert(1);
-    expect(countNodes(tree)).toEqual(8);
+	const tree = new BST();
+	tree.insert(7);
+	tree.insert(5);
+	tree.insert(13);
+	tree.insert(2);
+	tree.insert(6);
+	tree.insert(9);
+	tree.insert(15);
+	tree.insert(1);
+	expect(countNodes(tree.root)).toEqual(8);
 });
 /*
 
@@ -24,11 +26,13 @@ test("Return 8 for number of nodes in BST", () => {
 
 */
 
+// Test Case 2:
 test("Return 3 for number of nodes in BST", () => {
-    const tree = new Node(43);
-    tree.insert(25);
-    tree.insert(65);
-    expect(countNodes(tree)).toEqual(3);
+	const tree = new BST();
+	tree.insert(43);
+	tree.insert(25);
+	tree.insert(65);
+	expect(countNodes(tree.root)).toEqual(3);
 });
 /*
 
@@ -38,9 +42,11 @@ test("Return 3 for number of nodes in BST", () => {
 
 */
 
+// Test Case 3:
 test("Return 1 for number of nodes in BST", () => {
-    const tree = new Node(17);
-    expect(countNodes(tree)).toEqual(1);
+	const tree = new BST();
+	tree.insert(17)
+	expect(countNodes(tree.root)).toEqual(1);
 });
 /*
 
@@ -48,15 +54,17 @@ test("Return 1 for number of nodes in BST", () => {
 
 */
 
+// Test Case 4:
 test("Return 7 for number of nodes in BST", () => {
-    const tree = new Node(15);
-    tree.insert(17);
-    tree.insert(3);
-    tree.insert(21);
-    tree.insert(8);
-    tree.insert(2);
-    tree.insert(16);
-    expect(countNodes(tree)).toEqual(7);
+	const tree = new BST();
+	tree.insert(15);
+	tree.insert(17);
+	tree.insert(3);
+	tree.insert(21);
+	tree.insert(8);
+	tree.insert(2);
+	tree.insert(16);
+	expect(countNodes(tree.root)).toEqual(7);
 });
 /*
 
@@ -68,21 +76,24 @@ test("Return 7 for number of nodes in BST", () => {
 
 */
 
+// Test Case 5:
 test("Return 0 because there is no tree", () => {
-  expect(countNodes(null)).toEqual(0);
+	expect(countNodes(null)).toEqual(0);
 });
 
+// Test Case 6:
 test("Return 9 for number of nodes in BST", () => {
-    const tree = new Node(25);
-    tree.insert(16);
-    tree.insert(26);
-    tree.insert(5);
-    tree.insert(21);
-    tree.insert(35);
-    tree.insert(45);
-    tree.insert(47);
-    tree.insert(69);
-    expect(countNodes(tree)).toEqual(9);
+	const tree = new BST();
+	tree.insert(25);
+	tree.insert(16);
+	tree.insert(26);
+	tree.insert(5);
+	tree.insert(21);
+	tree.insert(35);
+	tree.insert(45);
+	tree.insert(47);
+	tree.insert(69);
+	expect(countNodes(tree.root)).toEqual(9);
 });
 /*
 
@@ -97,5 +108,20 @@ test("Return 9 for number of nodes in BST", () => {
                     47
                       \
                        69           
+
+*/
+
+// Test Case 7:
+test("Return 2 for number of nodes in BST", () => {
+	const tree = new BST();
+	tree.insert(5724);
+	tree.insert(1523);
+	expect(countNodes(tree.root)).toEqual(2);
+});
+/*
+
+  	   5724
+  	  /	  
+   1523      
 
 */
