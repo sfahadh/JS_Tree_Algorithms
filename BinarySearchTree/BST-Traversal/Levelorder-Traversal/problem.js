ae61de029d90b073b40786d36588c0cd7482d4e1
Fahad Hussain <sfahadh14@gmail.com>
2020-05-02 23:59:42 -0400

    adjusted and added 3 more test cases for levelorder traversal problem in bst

--- BinarySearchTree/BST-Traversal/Levelorder-Traversal/problem.js
+++ BinarySearchTree/BST-Traversal/Levelorder-Traversal/problem.js
@@ -1,5 +1,10 @@
 // ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
 
+/* // Uncomment the imports below if you plan on using them
+ * const BST = require("../../Starter/BST");
+ * const Node = require("../../Starter/Node");
+ */
+
 /*
 TODO: Complete the following level-order BST traversal problem below. The input is the whole tree structure starting from the root. The returned result must be an array. For level-order traversal, you must grab all the nodes in order from left to right on each level of the tree.
 
--- BinarySearchTree/BST-Traversal/Levelorder-Traversal/problem.test.js
+++ BinarySearchTree/BST-Traversal/Levelorder-Traversal/problem.test.js
@@ -1,11 +1,12 @@
-const Node = require("../../BST/starter");
+const BST = require("../../Starter/BST");
 const levelOrder = require("./problem");
 
 test("Return an array of values - [10, 6, 15]", () => {
-    const tree = new Node(10);
+    const tree = new BST();
+    tree.insert(10);
     tree.insert(6);
     tree.insert(15);
-    expect(levelOrder(tree)).toEqual([10, 6, 15]);
+    expect(levelOrder(tree.root)).toEqual([10, 6, 15]);
 });
 /*
 
@@ -16,8 +17,9 @@ test("Return an array of values - [10, 6, 15]", () => {
 */
 
 test("Return an array of value - [33]", () => {
-    const tree = new Node(33);
-    expect(levelOrder(tree)).toEqual([33]);
+    const tree = new BST();
+    tree.insert(33);
+    expect(levelOrder(tree.root)).toEqual([33]);
 });
 /*
 
@@ -26,14 +28,14 @@ test("Return an array of value - [33]", () => {
 */
 
 test("Return an array of values - [24, 17, 31, 34, 32, 39]", () => {
-    const tree = new Node(24);
+    const tree = new BST();
     tree.insert(24);
     tree.insert(17);
     tree.insert(31);
     tree.insert(34);
     tree.insert(32);
     tree.insert(39);
-    expect(levelOrder(tree)).toEqual([24, 17, 31, 34, 32, 39]);
+    expect(levelOrder(tree.root)).toEqual([24, 17, 31, 34, 32, 39]);
 });
 /*
 
@@ -52,16 +54,16 @@ test("Return an empty array", () => {
 });
 
 test("Return an array of values - [18, 5, 65, 3, 13, 33, 0]", () => {
-    const tree = new Node(18);
+    const tree = new BST();
+    tree.insert(18);
     tree.insert(5);
     tree.insert(13);
     tree.insert(3);
-    tree.insert(18);
     tree.insert(65);
     tree.insert(0);
     tree.insert(3);
     tree.insert(33);
-    expect(levelOrder(tree)).toEqual([18, 5, 65, 3, 13, 33, 0]);
+    expect(levelOrder(tree.root)).toEqual([18, 5, 65, 3, 13, 33, 0]);
 });
 /*
 
@@ -71,6 +73,58 @@ test("Return an array of values - [18, 5, 65, 3, 13, 33, 0]", () => {
     /  \   /   
    3   13 33 
   /         
-0          
+ 0          
+
+*/
+
+// Test Case 6:
+test("Return an array of values - [62, 123]", () => {
+    const tree = new BST();
+    tree.insert(62);
+    tree.insert(123);
+    expect(levelOrder(tree.root)).toEqual([62, 123]);
+});
+/*
+
+     62
+       \
+       123  
+
+*/
+
+// Test Case 7:
+test("Return an array of values - [62, 13]", () => {
+    const tree = new BST();
+    tree.insert(62);
+    tree.insert(13);
+    expect(levelOrder(tree.root)).toEqual([62, 13]);
+});
+/*
+
+     62
+    /   
+  13  
+
+*/
+
+// Test Case 8:
+test("Return an array of values - [6, 4, 8, 3, 5, 7, 9]", () => {
+    const tree = new BST();
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(8);
+    tree.insert(3);
+    tree.insert(5);
+    tree.insert(7);
+    tree.insert(9);
+    expect(levelOrder(tree.root)).toEqual([6, 4, 8, 3, 5, 7, 9]);
+});
+/*
+
+         6
+       /   \
+      4     8    
+    /  \   /  \
+   3    5 7    9   
 
 */
