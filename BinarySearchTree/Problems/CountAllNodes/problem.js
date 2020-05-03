c2e9702464cd4322bc184b196be86ace615fdaab
Fahad Hussain <sfahadh14@gmail.com>
2020-05-03 18:33:28 -0400

    added more content and highlighted comments in countAllNodes problem in BinarySearchTree

--- BinarySearchTree/Problems/CountAllNodes/problem.js
+++ BinarySearchTree/Problems/CountAllNodes/problem.js
@@ -1,5 +1,12 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BST = require("../../Starter/BST");
+ * const Node = require("../../Starter/Node");
+ */
+
 /*
-Complete the following BST count all nodes problem below. There is only 1 input and the goal is to return the total number of nodes found in the BST.
+TODO: Complete the following BST count all nodes problem below. There is only 1 input and the goal is to traverse through the tree and count the number of nodes the tree contains, then return that number.
 
 function BST() {
     this.root = null;
@@ -19,7 +26,8 @@ const countNodes = root => {
 module.exports = countNodes;
 
 /*
-
+! Test Case:
+Sample Input:
 	    25                   
 	   /  \             	   
      16    26      
