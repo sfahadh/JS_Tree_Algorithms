649cfc6077473cb876a09676251cbb27a9c4efd6
Fahad Hussain <sfahadh14@gmail.com>
2020-05-04 16:25:31 -0400

    added more content and highlighted comments in findMin problem in BinarySearchTree

--- BinarySearchTree/Problems/FindMin/problem.js
+++ BinarySearchTree/Problems/FindMin/problem.js
@@ -1,5 +1,12 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BST = require("../../Starter/BST");
+ * const Node = require("../../Starter/Node");
+ */
+
 /*
-Complete the following BST find min node problem below. You must traverse down the tree and find the node with the lowest value.
+TODO: Complete the following BST find min node problem below. There is only 1 input, which is the root of the BST and the goal is to traverse down the tree and find the node with the lowest value. Once found, return the node data. If there is no BST, return null;
 
 function BST() {
     this.root = null;
@@ -19,6 +26,7 @@ const findMin = root => {
 module.exports = findMin;
 
 /*
+! Test Case:
 
 	    25                          
 	   /  \             	   
@@ -26,5 +34,5 @@ module.exports = findMin;
     /  \     \           
    5   21     35             
 
-Max node = 5
+Min node = 5
 */
