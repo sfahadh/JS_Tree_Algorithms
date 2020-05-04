6ba9ac35ecba37da2571dc74a1e28dfd0c8f8e96
Fahad Hussain <sfahadh14@gmail.com>
2020-05-04 15:41:33 -0400

    added more content and highlighted comments in findHeight problem in BinarySearchTree

--- BinarySearchTree/Problems/FindHeight/problem.js
+++ BinarySearchTree/Problems/FindHeight/problem.js
@@ -1,5 +1,12 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BST = require("../../Starter/BST");
+ * const Node = require("../../Starter/Node");
+ */
+
 /*
-Complete the following BST find height problem below. There are 2 inputs, the root of the BST and the node. You must return the height of the node from the root of the BST.
+TODO: Complete the following BST find height problem below. There are 2 inputs, the root of the BST and the node that may or may not be located within the tree. The goal is to return the level or height in which the node can be found in.
 
 function BST() {
     this.root = null;
@@ -19,6 +26,7 @@ const findHeight = (root, node) => {
 module.exports = findHeight;
 
 /*
+Sample Tree Root:
 
 	    25         -> Height 1            
 	   /  \             	   
@@ -26,12 +34,15 @@ module.exports = findHeight;
     /  \     \           
    5   21     35   -> Height 3  
 
-Input: 25
-Output: 0
-
-Input: 16
+! Test Case 1:
+Input: (root, 25)
 Output: 1
 
-Input: 5
+! Test Case 2:
+Input: (root, 16)
 Output: 2
+
+! Test Case 3:
+Input: (root, 5)
+Output: 3
 */
