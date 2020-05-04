30b00d7462cece0f949232369d473f87d4998b88
Fahad Hussain <sfahadh14@gmail.com>
2020-05-04 17:27:56 -0400

    modified and added more test cases for numOfLevels problem

--- BinarySearchTree/Problems/NumOfLevels/problem.test.js
+++ BinarySearchTree/Problems/NumOfLevels/problem.test.js
@@ -1,16 +1,18 @@
-const Node = require("../../BST/starter");
+const BST = require("../../Starter/BST");
 const levels = require("./problem");
 
+// Test Case 1:
 test("The BST should return 4 as number of levels", () => {
-    const tree = new Node(7);
-    tree.insert(5);
-    tree.insert(13);
-    tree.insert(2);
-    tree.insert(6);
-    tree.insert(9);
-    tree.insert(15);
-    tree.insert(1);
-    expect(levels(tree)).toEqual(4);
+  const tree = new BST();
+  tree.insert(7);
+  tree.insert(5);
+  tree.insert(13);
+  tree.insert(2);
+  tree.insert(6);
+  tree.insert(9);
+  tree.insert(15);
+  tree.insert(1);
+  expect(levels(tree.root)).toEqual(4);
 });
 /*
 
@@ -24,11 +26,13 @@ test("The BST should return 4 as number of levels", () => {
 
 */
 
+// Test Case 2:
 test("The BST should return 2 as number of levels", () => {
-    const tree = new Node(43);
-    tree.insert(25);
-    tree.insert(65);
-    expect(levels(tree)).toEqual(2);
+  const tree = new BST();
+  tree.insert(43);
+  tree.insert(25);
+  tree.insert(65);
+  expect(levels(tree.root)).toEqual(2);
 });
 /*
 
@@ -38,9 +42,11 @@ test("The BST should return 2 as number of levels", () => {
 
 */
 
+// Test Case 3:
 test("The BST should return 1 as number of levels", () => {
-    const tree = new Node(17);
-    expect(levels(tree)).toEqual(1);
+  const tree = new BST();
+  tree.insert(17);
+  expect(levels(tree.root)).toEqual(1);
 });
 /*
 
@@ -48,15 +54,17 @@ test("The BST should return 1 as number of levels", () => {
 
 */
 
+// Test Case 4:
 test("The BST should return 3 as number of levels", () => {
-    const tree = new Node(15);
-    tree.insert(17);
-    tree.insert(3);
-    tree.insert(21);
-    tree.insert(8);
-    tree.insert(2);
-    tree.insert(16);
-    expect(levels(tree)).toEqual(3);
+  const tree = new BST();
+  tree.insert(15);
+  tree.insert(17);
+  tree.insert(3);
+  tree.insert(21);
+  tree.insert(8);
+  tree.insert(2);
+  tree.insert(16);
+  expect(levels(tree.root)).toEqual(3);
 });
 /*
 
@@ -68,26 +76,29 @@ test("The BST should return 3 as number of levels", () => {
 
 */
 
+// Test Case 5:
 test("The BST should return 0 as number of levels", () => {
   expect(levels(null)).toEqual(0);
 });
 
+// Test Case 6:
 test("The BST should return 6 as number of levels", () => {
-    const tree = new Node(25);
-    tree.insert(16);
-    tree.insert(26);
-    tree.insert(5);
-    tree.insert(21);
-    tree.insert(35);
-    tree.insert(45);
-    tree.insert(47);
-    tree.insert(69);
-    expect(levels(tree)).toEqual(6);
+  const tree = new BST();
+  tree.insert(25);
+  tree.insert(16);
+  tree.insert(26);
+  tree.insert(5);
+  tree.insert(21);
+  tree.insert(35);
+  tree.insert(45);
+  tree.insert(47);
+  tree.insert(69);
+  expect(levels(tree.root)).toEqual(6);
 });
 /*
 
-	    25                          
-	   /  \             	   
+	      25                          
+	     /  \             	   
      16    26     
     /  \     \           
    5   21     35  
@@ -98,4 +109,33 @@ test("The BST should return 6 as number of levels", () => {
                       \
                        69           
 
+*/
+
+// Test Case 6:
+test("The BST should return 6 as number of levels", () => {
+  const tree = new BST();
+  tree.insert(25);
+  tree.insert(26);
+  tree.insert(35);
+  tree.insert(45);
+  tree.insert(47);
+  tree.insert(69);
+  tree.insert(98);
+  expect(levels(tree.root)).toEqual(7);
+});
+/*
+
+  25                          
+    \             	   
+     26     
+       \           
+        35  
+          \
+           45
+             \
+              47
+                \
+                 69    
+                   \       
+                    98
 */
