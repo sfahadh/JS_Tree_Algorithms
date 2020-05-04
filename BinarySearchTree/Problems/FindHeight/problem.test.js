cf7371958b582fdee4ea88ddcff6e99fbcc023dd
Fahad Hussain <sfahadh14@gmail.com>
2020-05-04 15:48:57 -0400

    modified and added more test cases for findHeight problem

--- BinarySearchTree/Problems/FindHeight/problem.test.js
+++ BinarySearchTree/Problems/FindHeight/problem.test.js
@@ -1,16 +1,18 @@
-const Node = require("../../BST/starter");
+const BST = require("../../Starter/BST");
 const findHeight = require("./problem");
 
-test("From the root of the BST to the node, the height is 2", () => {
-    const tree = new Node(7);
-    tree.insert(5);
-    tree.insert(13);
-    tree.insert(2);
-    tree.insert(6);
-    tree.insert(9);
-    tree.insert(15);
-    tree.insert(1);
-    expect(findHeight(tree, 2)).toEqual(2);
+// Test Case 1:
+test("From the root of the BST to the node, the height is 3", () => {
+  const tree = new BST();
+  tree.insert(7);
+  tree.insert(5);
+  tree.insert(13);
+  tree.insert(2);
+  tree.insert(6);
+  tree.insert(9);
+  tree.insert(15);
+  tree.insert(1);
+  expect(findHeight(tree.root, 2)).toEqual(3);
 });
 /*
 
@@ -24,11 +26,13 @@ test("From the root of the BST to the node, the height is 2", () => {
 
 */
 
-test("From the root of the BST to the node, the height is 1", () => {
-    const tree = new Node(43);
-    tree.insert(25);
-    tree.insert(65);
-    expect(findHeight(tree, 25)).toEqual(1);
+// Test Case 2:
+test("From the root of the BST to the node, the height is 2", () => {
+  const tree = new BST();
+  tree.insert(43);
+  tree.insert(25);
+  tree.insert(65);
+  expect(findHeight(tree.root, 25)).toEqual(2);
 });
 /*
 
@@ -38,9 +42,11 @@ test("From the root of the BST to the node, the height is 1", () => {
 
 */
 
-test("From the root of the BST to the node, the height is 0", () => {
-    const tree = new Node(17);
-    expect(findHeight(tree, 17)).toEqual(0);
+// Test Case 3:
+test("From the root of the BST to the node, the height is 1", () => {
+  const tree = new BST();
+  tree.insert(17);
+  expect(findHeight(tree.root, 17)).toEqual(1);
 });
 /*
 
@@ -48,15 +54,17 @@ test("From the root of the BST to the node, the height is 0", () => {
 
 */
 
-test("Node not found in BST, should return 0", () => {
-    const tree = new Node(15);
-    tree.insert(17);
-    tree.insert(3);
-    tree.insert(21);
-    tree.insert(8);
-    tree.insert(2);
-    tree.insert(16);
-    expect(findHeight(tree, 24)).toEqual(0);
+// Test Case 4:
+test("Node not found in BST, should return 1", () => {
+  const tree = new BST();
+  tree.insert(15);
+  tree.insert(17);
+  tree.insert(3);
+  tree.insert(21);
+  tree.insert(8);
+  tree.insert(2);
+  tree.insert(16);
+  expect(findHeight(tree.root, 24)).toEqual(1);
 });
 /*
 
@@ -68,21 +76,24 @@ test("Node not found in BST, should return 0", () => {
 
 */
 
+// Test Case 5:
 test("Should return 0 as there is no tree", () => {
   expect(findHeight(null)).toEqual(0);
 });
 
-test("From the root of the BST to the node, the height is 5", () => {
-    const tree = new Node(25);
-    tree.insert(16);
-    tree.insert(26);
-    tree.insert(5);
-    tree.insert(21);
-    tree.insert(35);
-    tree.insert(45);
-    tree.insert(47);
-    tree.insert(69);
-    expect(findHeight(tree, 69)).toEqual(5);
+// Test Case 6:
+test("From the root of the BST to the node, the height is 6", () => {
+  const tree = new BST();
+  tree.insert(25);
+  tree.insert(16);
+  tree.insert(26);
+  tree.insert(5);
+  tree.insert(21);
+  tree.insert(35);
+  tree.insert(45);
+  tree.insert(47);
+  tree.insert(69);
+  expect(findHeight(tree.root, 69)).toEqual(6);
 });
 /*
 
@@ -100,13 +111,15 @@ test("From the root of the BST to the node, the height is 5", () => {
 
 */
 
-test("From the root of the BST to the node, the height is 2", () => {
-  const tree = new Node(3);
+// Test Case 7:
+test("From the root of the BST to the node, the height is 3", () => {
+  const tree = new BST();
+  tree.insert(3);
   tree.insert(2);
   tree.insert(4);
   tree.insert(1);
   tree.insert(5);
-  expect(findHeight(tree, 1)).toEqual(2);
+  expect(findHeight(tree.root, 1)).toEqual(3);
 });
 /*
 
@@ -115,4 +128,25 @@ test("From the root of the BST to the node, the height is 2", () => {
      2     4
     /       \
    1         5
+
+*/
+
+// Test Case 8:
+test("Node is not located anywhere in the BST", () => {
+  const tree = new BST();
+  tree.insert(3);
+  tree.insert(2);
+  tree.insert(4);
+  tree.insert(1);
+  tree.insert(5);
+  expect(findHeight(tree.root, 3214)).toEqual(0);
+});
+/*
+
+        3
+      /	  \
+     2     4
+    /       \
+   1         5
+   
 */
