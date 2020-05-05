4fb0869a81ccf47c7529f4b9de167780ce7f28bf
Fahad Hussain <sfahadh14@gmail.com>
2020-05-05 16:18:12 -0400

    added a constructTree function in removeALeaf test file

--- BinarySearchTree/Problems/RemoveALeaf/problem.test.js
+++ BinarySearchTree/Problems/RemoveALeaf/problem.test.js
@@ -1,16 +1,20 @@
-const Node = require("../../BST/starter");
+const BST = require("../../Starter/BST");
 const removeLeaf = require("./problem");
 
+const constructTree = nodes => {
+  const tree = new BST();
+  for (let node of nodes) {
+    tree.insert(node);
+  }
+  return tree;
+}
+
+// Test Case 1:
 test("return tree without the 89 leaf node", () => {
-    const tree = new Node(84);
-    tree.insert(66);
-    tree.insert(89);
-    const result = {
-        data: 84,
-        left: { data: 66, left: null, right: null },
-        right: null
-      }
-    expect(removeLeaf(tree, 89)).toEqual(result);
+  const inputTree = constructTree([84, 66, 89]);
+  const outputTree = constructTree([84, 66]);
+
+  expect(removeLeaf(inputTree, 89)).toEqual(outputTree);
 });
 
 /*
@@ -21,29 +25,47 @@ test("return tree without the 89 leaf node", () => {
 
 */
 
+// Test Case 2:
 test("return tree without the 1 leaf node", () => {
-    const tree = new Node(7);
-    tree.insert(5);
-    tree.insert(13);
-    tree.insert(2);
-    tree.insert(6);
-    tree.insert(9);
-    tree.insert(15);
-    tree.insert(1);
-    const result =  {
-        data: 7,
-        left: {
-          data: 5,
-          left: { data: 2, left: null, right: null },
-          right: { data: 6, left: null, right: null }
-        },
-        right: {
-          data: 13,
-          left: { data: 9, left: null, right: null },
-          right: { data: 15, left: null, right: null }
-        }
+  const tree = new BST();
+  tree.insert(7);
+  tree.insert(5);
+  tree.insert(13);
+  tree.insert(2);
+  tree.insert(6);
+  tree.insert(9);
+  tree.insert(15);
+  tree.insert(1);
+  const result = {
+    data: 7,
+    left: {
+      data: 5,
+      left: {
+        data: 2,
+        left: null,
+        right: null
+      },
+      right: {
+        data: 6,
+        left: null,
+        right: null
       }
-    expect(removeLeaf(tree, 1)).toEqual(result);
+    },
+    right: {
+      data: 13,
+      left: {
+        data: 9,
+        left: null,
+        right: null
+      },
+      right: {
+        data: 15,
+        left: null,
+        right: null
+      }
+    }
+  }
+  expect(removeLeaf(tree, 1)).toEqual(result);
 });
 
 /*
@@ -58,19 +80,21 @@ test("return tree without the 1 leaf node", () => {
 
 */
 
+// Test Case 3:
 test("return empty tree", () => {
-    expect(removeLeaf(null, 5)).toEqual(null);
+  expect(removeLeaf(null, 5)).toEqual(null);
 });
 
+// Test Case 4:
 test("Return original tree", () => {
-    const tree = new Node(24);
-    tree.insert(24);
-    tree.insert(17);
-    tree.insert(31);
-    tree.insert(34);
-    tree.insert(32);
-    tree.insert(39);
-    expect(removeLeaf(tree, 18)).toEqual(tree);
+  const tree = new BST();
+  tree.insert(24);
+  tree.insert(17);
+  tree.insert(31);
+  tree.insert(34);
+  tree.insert(32);
+  tree.insert(39);
+  expect(removeLeaf(tree, 18)).toEqual(tree);
 });
 
 /*
