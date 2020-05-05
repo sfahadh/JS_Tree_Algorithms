ca94dac371b285a0669647e38daa4dd430a2cdc6
Fahad Hussain <sfahadh14@gmail.com>
2020-05-05 17:24:58 -0400

    added more content and highlighted comments in sameParent problem in BinarySearchTree

--- BinarySearchTree/Problems/SameParent/problem.js
+++ BinarySearchTree/Problems/SameParent/problem.js
@@ -1,5 +1,12 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BST = require("../../Starter/BST");
+ * const Node = require("../../Starter/Node");
+ */
+
 /*
-Complete the following BST same parent problem below. There are 3 inputs, which are the root of the BST, and 2 nodes. You must return true if the 2 nodes have the same parent. 
+TODO: Complete the following BST same parent problem below. There are 3 inputs, which are the root of the BST, and 2 child nodes. Traverse down the tree and find the two child nodes, determine whether they have the same parent or not. If they do, return true, otherwise return false.
 
 function BST() {
     this.root = null;
@@ -19,19 +26,22 @@ const sameParent = (root, n1, n2) => {
 module.exports = sameParent;
 
 /*
-
-	       25                   
-	     /    \             	   
+Sample Tree Root:
+	     25                   
+	   /    \             	   
      16      26      
     /  \    /  \           
    5   21  24   35      
 
+! Test Case 1:
 Input: (root, 16, 26)
 Output: true
 
+! Test Case 2:
 Input: (root, 16, 24)
 Output: false
 
+! Test Case 3:
 Input: (root, 5, 24)
 Output: false
 */
