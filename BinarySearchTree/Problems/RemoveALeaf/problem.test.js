3863ac99fca6d369c050d5d6fcfd1f1a97189cc3
Fahad Hussain <sfahadh14@gmail.com>
2020-05-05 17:17:24 -0400

    added correct solution for removeALeaf problem in BinarySearchTree

--- BinarySearchTree/Problems/RemoveALeaf/problem.test.js
+++ BinarySearchTree/Problems/RemoveALeaf/problem.test.js
@@ -6,7 +6,7 @@ const removeLeaf = require("./problem");
 test("return tree without the 89 leaf node", () => {
   const inputTree = constructTree([84, 66, 89]);
   const outputTree = constructTree([84, 66]);
-  expect(removeLeaf(inputTree, 89)).toEqual(outputTree);
+  expect(removeLeaf(inputTree.root, 89)).toEqual(outputTree.root);
 });
 
 /*
@@ -21,7 +21,7 @@ test("return tree without the 89 leaf node", () => {
 test("return tree without the 1 leaf node", () => {
   const inputTree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
   const outputTree = constructTree([7, 5, 13, 2, 6, 9, 15]);
-  expect(removeLeaf(inputTree, 1)).toEqual(outputTree);
+  expect(removeLeaf(inputTree.root, 1)).toEqual(outputTree.root);
 });
 
 /*
@@ -44,7 +44,7 @@ test("return empty tree", () => {
 // Test Case 4:
 test("Return original tree - Part I", () => {
   const tree = constructTree([24, 17, 31, 34, 32, 39]);
-  expect(removeLeaf(tree, 18)).toEqual(tree);
+  expect(removeLeaf(tree.root, 18)).toEqual(tree.root);
 });
 
 /*
@@ -63,7 +63,7 @@ test("Return original tree - Part I", () => {
 test("Return the tree without the 17 leaf node", () => {
   const inputTree = constructTree([24, 17, 31, 34, 32, 39]);
   const outputTree = constructTree([24, 31, 34, 32, 39]);
-  expect(removeLeaf(inputTree, 17)).toEqual(outputTree);
+  expect(removeLeaf(inputTree.root, 17)).toEqual(outputTree.root);
 });
 
 /*
@@ -81,7 +81,7 @@ test("Return the tree without the 17 leaf node", () => {
 // Test Case 6:
 test("return tree without the 1234 leaf node", () => {
   const inputTree = constructTree([1234]);
-  expect(removeLeaf(inputTree, 1234)).toEqual(new BST());
+  expect(removeLeaf(inputTree.root, 1234)).toEqual(null);
 });
 
 /*
@@ -93,7 +93,7 @@ test("return tree without the 1234 leaf node", () => {
 // Test Case 7:
 test("Return original tree - Part II", () => {
   const tree = constructTree([54]);
-  expect(removeLeaf(tree, 53)).toEqual(tree);
+  expect(removeLeaf(tree.root, 53)).toEqual(tree.root);
 });
 
 /*
@@ -105,7 +105,7 @@ test("Return original tree - Part II", () => {
 // Test Case 8:
 test("return original tree because 7 is NOT a leaf node", () => {
   const tree = constructTree([7, 5, 13, 2, 6, 9, 15]);
-  expect(removeLeaf(tree, 7)).toEqual(tree);
+  expect(removeLeaf(tree.root, 7)).toEqual(tree.root);
 });
 
 /*
@@ -121,7 +121,7 @@ test("return original tree because 7 is NOT a leaf node", () => {
 // Test Case 9:
 test("return original tree because 66 is NOT a leaf node", () => {
   const tree = constructTree([84, 66, 89, 77, 99]);
-  expect(removeLeaf(tree, 66)).toEqual(tree);
+  expect(removeLeaf(tree.root, 66)).toEqual(tree.root);
 });
 
 /*
--- BinarySearchTree/Problems/RemoveALeaf/solution.js
+++ BinarySearchTree/Problems/RemoveALeaf/solution.js
@@ -1,3 +1,24 @@
 const removeLeaf = (root, leaf) => {
+    if (root === null) return root;
+    if (root && !root.left && !root.right && root.data === leaf) return null;
 
+    if (root.left) {
+        if (root.left.data === leaf) {
+            root.left = null;
+            return root;
+        } else {
+            removeLeaf(root.left, leaf);
+        }
+    }
+
+    if (root.right) {
+        if (root.right.data === leaf) {
+            root.right = null;
+            return root;
+        } else {
+            removeLeaf(root.right, leaf);
+        }
+    }
+
+    return root;
 }
