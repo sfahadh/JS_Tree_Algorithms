5f9b216b67ca1b5c358a9b6c2d414d94e84ea447
Fahad Hussain <sfahadh14@gmail.com>
2020-05-06 14:50:12 -0400

    modified and added more test cases for sumAllNodes problem in BinarySearchTree

--- BinarySearchTree/Problems/SumAllNodes/problem.test.js
+++ BinarySearchTree/Problems/SumAllNodes/problem.test.js
@@ -1,16 +1,10 @@
-const Node = require("../../BST/starter");
+const constructTree = require("../../Starter/ConstructTree");
 const sum = require("./problem");
 
+// Test Case 1:
 test("The sum of all nodes should return 58", () => {
-    const tree = new Node(7);
-    tree.insert(5);
-    tree.insert(13);
-    tree.insert(2);
-    tree.insert(6);
-    tree.insert(9);
-    tree.insert(15);
-    tree.insert(1);
-    expect(sum(tree)).toEqual(58);
+  const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
+  expect(sum(tree.root)).toEqual(58);
 });
 /*
 
@@ -24,11 +18,13 @@ test("The sum of all nodes should return 58", () => {
 
 */
 
+// Test Case 2:
 test("The sum of all nodes should return 133", () => {
-    const tree = new Node(43);
-    tree.insert(25);
-    tree.insert(65);
-    expect(sum(tree)).toEqual(133);
+  const tree = constructTree([43, 25, 65]);
+  tree.insert(43);
+  tree.insert(25);
+  tree.insert(65);
+  expect(sum(tree.root)).toEqual(133);
 });
 /*
 
@@ -38,9 +34,10 @@ test("The sum of all nodes should return 133", () => {
 
 */
 
+// Test Case 3:
 test("The sum of all nodes should return 17", () => {
-    const tree = new Node(17);
-    expect(sum(tree)).toEqual(17);
+  const tree = constructTree([17]);
+  expect(sum(tree.root)).toEqual(17);
 });
 /*
 
@@ -48,15 +45,10 @@ test("The sum of all nodes should return 17", () => {
 
 */
 
+// Test Case 4:
 test("The sum of all nodes should return 82", () => {
-    const tree = new Node(15);
-    tree.insert(17);
-    tree.insert(3);
-    tree.insert(21);
-    tree.insert(8);
-    tree.insert(2);
-    tree.insert(16);
-    expect(sum(tree)).toEqual(82);
+  const tree = constructTree([15, 3, 17, 2, 8, 16, 21])
+  expect(sum(tree.root)).toEqual(82);
 });
 /*
 
@@ -68,34 +60,41 @@ test("The sum of all nodes should return 82", () => {
 
 */
 
+// Test Case 5:
 test("The sum of all nodes should return 0", () => {
   expect(sum(null)).toEqual(0);
 });
 
+// Test Case 6:
 test("The sum of all nodes should return 289", () => {
-    const tree = new Node(25);
-    tree.insert(16);
-    tree.insert(26);
-    tree.insert(5);
-    tree.insert(21);
-    tree.insert(35);
-    tree.insert(45);
-    tree.insert(47);
-    tree.insert(69);
-    expect(sum(tree)).toEqual(289);
+  const tree = constructTree([25, 16, 26, 5, 21, 35, 45, 47, 69]);
+  expect(sum(tree.root)).toEqual(289);
 });
 /*
 
 	    25                          
 	   /  \             	   
-    16    26     
-   /  \     \           
-  5   21     35  
-                \
-                 45
-                   \
-                    47
-                      \
-                       69           
+    16   26     
+   /  \    \           
+  5   21    35  
+              \
+               45
+                 \
+                  47
+                    \
+                     69           
+
+*/
+
+// Test Case 7:
+test("The sum of all nodes should return 1", () => {
+  const tree = constructTree([1, 0]);
+  expect(sum(tree.root)).toEqual(1);
+});
+/*
+
+  	  1
+  	/	 
+  0      
 
 */
