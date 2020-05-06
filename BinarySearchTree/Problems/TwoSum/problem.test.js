7ad203a7fc291e6d545d45d2b447caf0325ecd17
Fahad Hussain <sfahadh14@gmail.com>
2020-05-06 15:46:14 -0400

    modified and added more test cases for twoSums problem in BinarySearchTree

--- BinarySearchTree/Problems/TwoSum/problem.test.js
+++ BinarySearchTree/Problems/TwoSum/problem.test.js
@@ -1,16 +1,10 @@
 const constructTree = require("../../Starter/ConstructTree");
 const twoSum = require("./problem");
 
-test("Should return true, there are 2 nodes that adds up to the value", () => {
-    const tree = new Node(7);
-    tree.insert(5);
-    tree.insert(13);
-    tree.insert(2);
-    tree.insert(6);
-    tree.insert(9);
-    tree.insert(15);
-    tree.insert(1);
-    expect(twoSum(tree, 15)).toEqual(true);
+// Test Case 1:
+test("Return true because there are multiple 2 nodes that can be added to 15", () => {
+    const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 0]);
+    expect(twoSum(tree.root, 15)).toEqual(true);
 });
 /*
 
@@ -20,15 +14,14 @@ test("Should return true, there are 2 nodes that adds up to the value", () => {
      /  \   /  \
     2    6 9   15
    /
-  1 
+  0 
 
 */
 
-test("Should return false, there are no nodes that adds up to the value", () => {
-    const tree = new Node(43);
-    tree.insert(25);
-    tree.insert(65);
-    expect(twoSum(tree, 50)).toEqual(false);
+// Test Case 2:
+test("Return false because there are no nodes that adds up to 50", () => {
+    const tree = constructTree([43, 25, 65]);
+    expect(twoSum(tree.root, 50)).toEqual(false);
 });
 /*
 
@@ -38,9 +31,10 @@ test("Should return false, there are no nodes that adds up to the value", () => 
 
 */
 
-test("Should return false, there is only 1 node in the tree", () => {
-    const tree = new Node(17);
-    expect(twoSum(tree, 17)).toEqual(false);
+// Test Case 3:
+test("Return false there is only 1 node in the tree", () => {
+    const tree = constructTree([17]);
+    expect(twoSum(tree.root, 17)).toEqual(false);
 });
 /*
 
@@ -48,15 +42,10 @@ test("Should return false, there is only 1 node in the tree", () => {
 
 */
 
-test("Should return true, there are 2 nodes that adds up to the value", () => {
-    const tree = new Node(15);
-    tree.insert(17);
-    tree.insert(3);
-    tree.insert(21);
-    tree.insert(8);
-    tree.insert(2);
-    tree.insert(16);
-    expect(twoSum(tree, 18)).toEqual(true);
+// Test Case 4:
+test("Return true because there are 2 nodes that adds up to 18", () => {
+    const tree = constructTree([15, 3, 17, 2, 8, 16, 21]);
+    expect(twoSum(tree.root, 18)).toEqual(true);
 });
 /*
 
@@ -68,21 +57,15 @@ test("Should return true, there are 2 nodes that adds up to the value", () => {
 
 */
 
-test("Should return false, there is no tree", () => {
+// Test Case 5:
+test("Return false because there is no tree", () => {
     expect(twoSum(null, 10)).toEqual(false);
 });
 
-test("Should return true, there are 2 nodes that adds up to the value", () => {
-    const tree = new Node(25);
-    tree.insert(16);
-    tree.insert(26);
-    tree.insert(5);
-    tree.insert(21);
-    tree.insert(35);
-    tree.insert(45);
-    tree.insert(47);
-    tree.insert(69);
-    expect(twoSum(tree, 74)).toEqual(true);
+// Test Case 6:
+test("Return true because there are 2 nodes that adds up to 74", () => {
+    const tree = constructTree([25, 16, 26, 5, 21, 35, 45, 47, 69]);
+    expect(twoSum(tree.root, 74)).toEqual(true);
 });
 /*
 
@@ -97,5 +80,61 @@ test("Should return true, there are 2 nodes that adds up to the value", () => {
                     47
                       \
                        69           
+
+*/
+
+// Test Case 7:
+test("Return true because there are 2 nodes that adds up to 1", () => {
+    const tree = constructTree([1, 0]);
+    expect(twoSum(tree.root, 1)).toEqual(true);
+});
+/*
+
+  	   1
+  	 /	 
+   0      
+
+*/
+
+// Test Case 8:
+test("Return true because there are 2 nodes that adds up to 3", () => {
+    const tree = constructTree([1, 2]);
+    expect(twoSum(tree.root, 3)).toEqual(true);
+});
+/*
+
+   1
+    \
+     2
+
+*/
+
+// Test Case 9:
+test("Return false because there are no nodes that adds up to 125312341234", () => {
+    const tree = constructTree([7, 5, 13, 2, 6, 9, 15]);
+    expect(twoSum(tree.root, 125312341234)).toEqual(false);
+});
+/*
+
+  		  7
+  	    /  	\
+       5     13
+     /  \   /  \
+    2    6 9   15
+
+*/
+
+// Test Case 10:
+test("Return false because there are no nodes that adds up to 7", () => {
+    const tree = constructTree([8, 5, 13, 2, 6, 9, 15]);
+    expect(twoSum(tree.root, 9)).toEqual(false);
+});
+/*
+
+  		  8
+  	    /  	\
+       5     13
+     /  \   /  \
+    2    6 9   15
 
 */
