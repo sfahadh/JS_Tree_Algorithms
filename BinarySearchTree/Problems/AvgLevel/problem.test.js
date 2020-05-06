34b636860262e5864b17394aaa69ae11d7bd322d
Fahad Hussain <sfahadh14@gmail.com>
2020-05-06 16:09:41 -0400

    adjusted test file and imported constructTree function for avgLevel

--- BinarySearchTree/Problems/AvgLevel/problem.test.js
+++ BinarySearchTree/Problems/AvgLevel/problem.test.js
@@ -1,22 +1,14 @@
-const BST = require("../../Starter/BST");
+const constructTree = require("../../Starter/ConstructTree");
 const avgLevel = require("./problem");
 
 // Test Case 1:
 test("The average levels of BST are [7, 9, 8, 1]", () => {
-  const tree = new BST();
-  tree.insert(7);
-  tree.insert(5);
-  tree.insert(13);
-  tree.insert(2);
-  tree.insert(6);
-  tree.insert(9);
-  tree.insert(15);
-  tree.insert(1);
-  expect(avgLevel(tree.root)).toEqual([7, 9, 8, 1]);
+	const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
+	expect(avgLevel(tree.root)).toEqual([7, 9, 8, 1]);
 });
 /*
 
-  		    7
+  		  7
   	    /  	\
        5     13
      /  \   /  \
@@ -28,11 +20,8 @@ test("The average levels of BST are [7, 9, 8, 1]", () => {
 
 // Test Case 2:
 test("The average levels of BST are [43, 45]", () => {
-  const tree = new BST();
-  tree.insert(43);
-  tree.insert(25);
-  tree.insert(65);
-  expect(avgLevel(tree.root)).toEqual([43, 45]);
+	const tree = constructTree([43, 25, 65]);
+	expect(avgLevel(tree.root)).toEqual([43, 45]);
 });
 /*
 
@@ -44,9 +33,8 @@ test("The average levels of BST are [43, 45]", () => {
 
 // Test Case 3:
 test("The average levels of BST are [17]", () => {
-  const tree = new BST();
-  tree.insert(17);
-  expect(avgLevel(tree.root)).toEqual([17]);
+	const tree = constructTree([17]);
+	expect(avgLevel(tree.root)).toEqual([17]);
 });
 /*
 
@@ -56,15 +44,8 @@ test("The average levels of BST are [17]", () => {
 
 // Test Case 4:
 test("The average levels of BST are [15, 10, 12]", () => {
-  const tree = new BST();
-  tree.insert(15);
-  tree.insert(17);
-  tree.insert(3);
-  tree.insert(21);
-  tree.insert(9);
-  tree.insert(2);
-  tree.insert(16);
-  expect(avgLevel(tree.root)).toEqual([15, 10, 12]);
+	const tree = constructTree([15, 3, 17, 2, 9, 16, 21]);
+	expect(avgLevel(tree.root)).toEqual([15, 10, 12]);
 });
 /*
 
@@ -78,27 +59,18 @@ test("The average levels of BST are [15, 10, 12]", () => {
 
 // Test Case 5:
 test("The average levels of BST are []", () => {
-  expect(avgLevel(null)).toEqual([]);
+	expect(avgLevel(null)).toEqual([]);
 });
 
 // Test Case 6:
 test("The average levels of BST are [25, 21, 20, 45, 47, 69]", () => {
-  const tree = new BST();
-  tree.insert(25);
-  tree.insert(16);
-  tree.insert(26);
-  tree.insert(4);
-  tree.insert(21);
-  tree.insert(35);
-  tree.insert(45);
-  tree.insert(47);
-  tree.insert(69);
-  expect(avgLevel(tree.root)).toEqual([25, 21, 20, 45, 47, 69]);
+	const tree = constructTree([25, 16, 26, 4, 21, 35, 45, 47, 69]);
+	expect(avgLevel(tree.root)).toEqual([25, 21, 20, 45, 47, 69]);
 });
 /*
 
-	      25                          
-	     /  \             	   
+	    25                          
+	   /  \             	   
      16    26     
     /  \     \           
    4   21     35  
@@ -113,25 +85,21 @@ test("The average levels of BST are [25, 21, 20, 45, 47, 69]", () => {
 
 // Test Case 7:
 test("The average levels of BST are [69, 96]", () => {
-  const tree = new BST();
-  tree.insert(69);
-  tree.insert(96);
-  expect(avgLevel(tree.root)).toEqual([69, 96]);
+	const tree = constructTree([69, 96]);
+	expect(avgLevel(tree.root)).toEqual([69, 96]);
 });
 /*
 
   	   69
-  	 	   \
+  	 	 \
           96
 
 */
 
 // Test Case 8:
 test("The average levels of BST are [96, 69]", () => {
-  const tree = new BST();
-  tree.insert(96);
-  tree.insert(69);
-  expect(avgLevel(tree.root)).toEqual([96, 69]);
+	const tree = constructTree([96, 69]);
+	expect(avgLevel(tree.root)).toEqual([96, 69]);
 });
 /*
 
--- BinarySearchTree/Problems/SumAllNodes/problem.test.js
+++ BinarySearchTree/Problems/SumAllNodes/problem.test.js
@@ -3,8 +3,8 @@ const sum = require("./problem");
 
 // Test Case 1:
 test("The sum of all nodes should return 58", () => {
-  const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
-  expect(sum(tree.root)).toEqual(58);
+    const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
+    expect(sum(tree.root)).toEqual(58);
 });
 /*
 
@@ -20,8 +20,8 @@ test("The sum of all nodes should return 58", () => {
 
 // Test Case 2:
 test("The sum of all nodes should return 133", () => {
-  const tree = constructTree([43, 25, 65]);
-  expect(sum(tree.root)).toEqual(133);
+    const tree = constructTree([43, 25, 65]);
+    expect(sum(tree.root)).toEqual(133);
 });
 /*
 
@@ -33,8 +33,8 @@ test("The sum of all nodes should return 133", () => {
 
 // Test Case 3:
 test("The sum of all nodes should return 17", () => {
-  const tree = constructTree([17]);
-  expect(sum(tree.root)).toEqual(17);
+    const tree = constructTree([17]);
+    expect(sum(tree.root)).toEqual(17);
 });
 /*
 
@@ -44,8 +44,8 @@ test("The sum of all nodes should return 17", () => {
 
 // Test Case 4:
 test("The sum of all nodes should return 82", () => {
-  const tree = constructTree([15, 3, 17, 2, 8, 16, 21])
-  expect(sum(tree.root)).toEqual(82);
+    const tree = constructTree([15, 3, 17, 2, 8, 16, 21]);
+    expect(sum(tree.root)).toEqual(82);
 });
 /*
 
@@ -59,18 +59,18 @@ test("The sum of all nodes should return 82", () => {
 
 // Test Case 5:
 test("The sum of all nodes should return 0", () => {
-  expect(sum(null)).toEqual(0);
+    expect(sum(null)).toEqual(0);
 });
 
 // Test Case 6:
 test("The sum of all nodes should return 289", () => {
-  const tree = constructTree([25, 16, 26, 5, 21, 35, 45, 47, 69]);
-  expect(sum(tree.root)).toEqual(289);
+    const tree = constructTree([25, 16, 26, 5, 21, 35, 45, 47, 69]);
+    expect(sum(tree.root)).toEqual(289);
 });
 /*
 
-	    25                          
-	   /  \             	   
+	  25                          
+	 /  \             	   
     16   26     
    /  \    \           
   5   21    35  
@@ -85,8 +85,8 @@ test("The sum of all nodes should return 289", () => {
 
 // Test Case 7:
 test("The sum of all nodes should return 1", () => {
-  const tree = constructTree([1, 0]);
-  expect(sum(tree.root)).toEqual(1);
+    const tree = constructTree([1, 0]);
+    expect(sum(tree.root)).toEqual(1);
 });
 /*
 
