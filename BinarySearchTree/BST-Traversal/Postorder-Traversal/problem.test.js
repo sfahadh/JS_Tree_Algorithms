e87454531182f3871dfa06b568de57073fb3a036
Fahad Hussain <sfahadh14@gmail.com>
2020-05-06 16:00:28 -0400

    adjusted test file and imported constructTree function for postorder-traversal

--- BinarySearchTree/BST-Traversal/Postorder-Traversal/problem.test.js
+++ BinarySearchTree/BST-Traversal/Postorder-Traversal/problem.test.js
@@ -1,12 +1,10 @@
 const BST = require("../../Starter/BST");
+const constructTree = require("../../Starter/ConstructTree");
 const postOrder = require("./problem");
 
 // Test Case 1:
 test("Return an array of values - [10, 6, 15]", () => {
-    const tree = new BST();
-    tree.insert(10);
-    tree.insert(6);
-    tree.insert(15);
+    const tree = constructTree([10, 6, 15]);
     expect(postOrder(tree.root)).toEqual([6, 15, 10]);
 });
 /*
@@ -19,8 +17,7 @@ test("Return an array of values - [10, 6, 15]", () => {
 
 // Test Case 2:
 test("Return an array of value - [33]", () => {
-    const tree = new BST();
-    tree.insert(33);
+    const tree = constructTree([33]);
     expect(postOrder(tree.root)).toEqual([33]);
 });
 /*
@@ -31,13 +28,7 @@ test("Return an array of value - [33]", () => {
 
 // Test Case 3:
 test("Return an array of values - [17, 32, 39, 34, 31, 24]", () => {
-    const tree = new BST();
-    tree.insert(24);
-    tree.insert(17);
-    tree.insert(31);
-    tree.insert(34);
-    tree.insert(32);
-    tree.insert(39);
+    const tree = constructTree([24, 17, 31, 34, 32, 39]);
     expect(postOrder(tree.root)).toEqual([17, 32, 39, 34, 31, 24]);
 });
 /*
@@ -59,15 +50,7 @@ test("Return an empty array", () => {
 
 // Test Case 5:
 test("Return an array of values - [0, 3, 13, 5, 33, 65, 18]", () => {
-    const tree = new BST();
-    tree.insert(18);
-    tree.insert(5);
-    tree.insert(13);
-    tree.insert(3);
-    tree.insert(65);
-    tree.insert(0);
-    tree.insert(3);
-    tree.insert(33);
+    const tree = constructTree([18, 5, 65, 3, 13, 33, 0]);
     expect(postOrder(tree.root)).toEqual([0, 3, 13, 5, 33, 65, 18]);
 });
 /*
@@ -84,9 +67,7 @@ test("Return an array of values - [0, 3, 13, 5, 33, 65, 18]", () => {
 
 // Test Case 6:
 test("Return an array of values - [123, 62]", () => {
-    const tree = new BST();
-    tree.insert(62);
-    tree.insert(123);
+    const tree = constructTree([62, 123]);
     expect(postOrder(tree.root)).toEqual([123, 62]);
 });
 /*
@@ -99,9 +80,7 @@ test("Return an array of values - [123, 62]", () => {
 
 // Test Case 7:
 test("Return an array of values - [13, 62]", () => {
-    const tree = new BST();
-    tree.insert(62);
-    tree.insert(13);
+    const tree = constructTree([62, 13]);
     expect(postOrder(tree.root)).toEqual([13, 62]);
 });
 /*
@@ -114,14 +93,7 @@ test("Return an array of values - [13, 62]", () => {
 
 // Test Case 8:
 test("Return an array of values - [3, 5, 4, 7, 9, 8, 6]", () => {
-    const tree = new BST();
-    tree.insert(6);
-    tree.insert(4);
-    tree.insert(8);
-    tree.insert(3);
-    tree.insert(5);
-    tree.insert(7);
-    tree.insert(9);
+    const tree = constructTree([6, 4, 8, 3, 5, 7, 9]);
     expect(postOrder(tree.root)).toEqual([3, 5, 4, 7, 9, 8, 6]);
 });
 /*
