b8c2c7dc789dca7338ab4ce39c6c8e350b26a3cf
Fahad Hussain <sfahadh14@gmail.com>
2020-05-07 01:52:31 -0400

    adjusted test file and imported constructTree function for findMax

--- BinarySearchTree/Problems/FindHeight/problem.test.js
+++ BinarySearchTree/Problems/FindHeight/problem.test.js
@@ -3,12 +3,12 @@ const findHeight = require("./problem");
 
 // Test Case 1:
 test("From the root of the BST to the node, the height is 3 - Part I", () => {
-	const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
-	expect(findHeight(tree.root, 2)).toEqual(3);
+  const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
+  expect(findHeight(tree.root, 2)).toEqual(3);
 });
 /*
 
-  		  7
+  		    7
   	    /  	\
        5     13
      /  \   /  \
@@ -20,8 +20,8 @@ test("From the root of the BST to the node, the height is 3 - Part I", () => {
 
 // Test Case 2:
 test("From the root of the BST to the node, the height is 2", () => {
-	const tree = constructTree([43, 25, 65]);
-	expect(findHeight(tree.root, 25)).toEqual(2);
+  const tree = constructTree([43, 25, 65]);
+  expect(findHeight(tree.root, 25)).toEqual(2);
 });
 /*
 
@@ -33,8 +33,8 @@ test("From the root of the BST to the node, the height is 2", () => {
 
 // Test Case 3:
 test("From the root of the BST to the node, the height is 1", () => {
-	const tree = constructTree([17]);
-	expect(findHeight(tree.root, 17)).toEqual(1);
+  const tree = constructTree([17]);
+  expect(findHeight(tree.root, 17)).toEqual(1);
 });
 /*
 
@@ -44,8 +44,8 @@ test("From the root of the BST to the node, the height is 1", () => {
 
 // Test Case 4:
 test("Node not found in BST, should return 0", () => {
-	const tree = constructTree([15, 3, 17, 2, 8, 16, 21]);
-	expect(findHeight(tree.root, 24)).toEqual(0);
+  const tree = constructTree([15, 3, 17, 2, 8, 16, 21]);
+  expect(findHeight(tree.root, 24)).toEqual(0);
 });
 /*
 
@@ -59,18 +59,18 @@ test("Node not found in BST, should return 0", () => {
 
 // Test Case 5:
 test("Should return 0 as there is no tree", () => {
-	expect(findHeight(null)).toEqual(0);
+  expect(findHeight(null)).toEqual(0);
 });
 
 // Test Case 6:
 test("From the root of the BST to the node, the height is 6", () => {
-	const tree = constructTree([25, 16, 26, 5, 21, 35, 45, 47, 69]);
-	expect(findHeight(tree.root, 69)).toEqual(6);
+  const tree = constructTree([25, 16, 26, 5, 21, 35, 45, 47, 69]);
+  expect(findHeight(tree.root, 69)).toEqual(6);
 });
 /*
 
-	    25                          
-	  /   \             	   
+	      25                          
+	    /   \             	   
      16    26     
     /  \     \           
    5   21     35  
@@ -85,8 +85,8 @@ test("From the root of the BST to the node, the height is 6", () => {
 
 // Test Case 7:
 test("From the root of the BST to the node, the height is 3 - Part II", () => {
-	const tree = constructTree([3, 2, 4, 1, 6]);
-	expect(findHeight(tree.root, 1)).toEqual(3);
+  const tree = constructTree([3, 2, 4, 1, 6]);
+  expect(findHeight(tree.root, 1)).toEqual(3);
 });
 /*
 
@@ -100,8 +100,8 @@ test("From the root of the BST to the node, the height is 3 - Part II", () => {
 
 // Test Case 8:
 test("Node is not located anywhere in the BST", () => {
-	const tree = constructTree([3, 2, 4, 1, 6]);
-	expect(findHeight(tree.root, 3214)).toEqual(0);
+  const tree = constructTree([3, 2, 4, 1, 6]);
+  expect(findHeight(tree.root, 3214)).toEqual(0);
 });
 /*
 
--- BinarySearchTree/Problems/FindMax/problem.test.js
+++ BinarySearchTree/Problems/FindMax/problem.test.js
@@ -1,23 +1,15 @@
-const BST = require("../../Starter/BST");
+const constructTree = require("../../Starter/ConstructTree");
 const findMax = require("./problem");
 
 // Test Case 1:
 test("Highest node value in tree is 15", () => {
-  const tree = new BST();
-  tree.insert(7);
-  tree.insert(5);
-  tree.insert(13);
-  tree.insert(2);
-  tree.insert(6);
-  tree.insert(9);
-  tree.insert(15);
-  tree.insert(1);
-  expect(findMax(tree.root)).toEqual(15);
+	const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
+	expect(findMax(tree.root)).toEqual(15);
 });
 /*
 
-  		    7 
-  	    /	  \
+  		  7 
+  	    /	\
        5     13
      /  \   /  \
     2    6 9   15
@@ -28,11 +20,8 @@ test("Highest node value in tree is 15", () => {
 
 // Test Case 2:
 test("Highest node value in tree is 65", () => {
-  const tree = new BST();
-  tree.insert(43);
-  tree.insert(25);
-  tree.insert(65);
-  expect(findMax(tree.root)).toEqual(65);
+	const tree = constructTree([43, 25, 65]);
+	expect(findMax(tree.root)).toEqual(65);
 });
 /*
 
@@ -44,12 +33,8 @@ test("Highest node value in tree is 65", () => {
 
 // Test Case 3:
 test("Highest node value in tree is 27", () => {
-  const tree = new BST();
-  tree.insert(17);
-  tree.insert(12);
-  tree.insert(3);
-  tree.insert(27);
-  expect(findMax(tree.root)).toEqual(27);
+	const tree = constructTree([17, 3, 12, 27]);
+	expect(findMax(tree.root)).toEqual(27);
 });
 /*
 
@@ -63,15 +48,8 @@ test("Highest node value in tree is 27", () => {
 
 // Test Case 4:
 test("Highest node value in tree is 21", () => {
-  const tree = new BST();
-  tree.insert(15);
-  tree.insert(17);
-  tree.insert(3);
-  tree.insert(21);
-  tree.insert(8);
-  tree.insert(2);
-  tree.insert(16);
-  expect(findMax(tree.root)).toEqual(21);
+	const tree = constructTree([15, 3, 17, 2, 8, 16, 21]);
+	expect(findMax(tree.root)).toEqual(21);
 });
 /*
 
@@ -85,24 +63,18 @@ test("Highest node value in tree is 21", () => {
 
 // Test Case 5:
 test("There is no tree, should return null", () => {
-  expect(findMax(null)).toEqual(null);
+	expect(findMax(null)).toEqual(null);
 });
 
 // Test Case 6:
 test("Highest node value in tree is 35", () => {
-  const tree = new BST();
-  tree.insert(25);
-  tree.insert(16);
-  tree.insert(26);
-  tree.insert(5);
-  tree.insert(21);
-  tree.insert(35);
-  expect(findMax(tree.root)).toEqual(35);
+	const tree = constructTree([25, 16, 26, 5, 21, 35]);
+	expect(findMax(tree.root)).toEqual(35);
 });
 /*
 
-	      25                          
-	     /  \             	   
+	    25                          
+	   /  \             	   
      16    26     
     /  \     \           
    5   21     35             
@@ -111,9 +83,8 @@ test("Highest node value in tree is 35", () => {
 
 // Test Case 7:
 test("Highest node value in tree is 545315", () => {
-  const tree = new BST();
-  tree.insert(545315);
-  expect(findMax(tree.root)).toEqual(545315);
+	const tree = constructTree([545315]);
+	expect(findMax(tree.root)).toEqual(545315);
 });
 /*
 
