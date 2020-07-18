22dac795d5ef6196c44c49791d2ef67378fb0e9f
Fahad Hussain <sfahadh14@gmail.com>
2020-07-18 18:39:31 -0400

    moved insert function of max binary heap to its own file

--- MaxBinaryHeap/Starter/MaxBinaryHeap.js
+++ MaxBinaryHeap/Starter/MaxBinaryHeap.js
@@ -2,26 +2,4 @@ function MaxBinaryHeap() {
     this.values = [];
 }
 
-MaxBinaryHeap.prototype.insert = function (data) {
-    this.values.push(data);
-    this.bubbleUp();
-}
-
-MaxBinaryHeap.prototype.bubbleUp = function () {
-    let index = this.values.length - 1;
-    const element = this.values[index];
-    let parentIndex;
-    let parent;
-
-    while (index > 0) {
-        parentIndex = Math.floor((index - 1) / 2);
-        parent = this.values[parentIndex];
-        if (element <= parent) break;
-
-        this.values[parentIndex] = element;
-        this.values[index] = parent;
-        index = parentIndex;
-    }
-}
-
 module.exports = MaxBinaryHeap;
