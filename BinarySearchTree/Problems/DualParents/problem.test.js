486163928485a67a24eeda586acdbac34c191eab
Fahad <sfahad_hus@yahoo.com>
2020-08-11 15:55:30 -0400

    added 11th image of BST structure

--- BinarySearchTree/Problems/DualParents/problem.test.js
+++ BinarySearchTree/Problems/DualParents/problem.test.js
@@ -30,26 +30,11 @@ test("Should return 0 because there is no tree", () => {
 	expect(dualParent(null)).toEqual(0);
 });
 
-// Test Case 6:
+// Test Case 6: //* Sample Input: refer to Images -> BST-structure-11.png
 test("There are 3 parents with a left and right child", () => {
 	const tree = constrctTree([25, 16, 30, 5, 20, 35, 45, 42, 50, 69]);
 	expect(dualParent(tree.root)).toEqual(3);
 });
-/*
-
-	   25                          
-      /  \             	   
-    15    30     
-   /  \     \           
-  5   20     35  
-                \
-                 45
-                /  \
-              42    50
-                      \
-                       69           
-
-*/
 
 // Test Case 7: //* Sample Input: refer to Images -> BST-structure-4.png
 test("There are 0 parents with a left and right child", () => {
