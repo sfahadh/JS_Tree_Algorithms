21051430ffe92564ae3c5aa46b1a2529d07e591b
Fahad <sfahad_hus@yahoo.com>
2020-08-18 20:50:27 -0400

    changed order of test cases in level order traversal problem in BST

--- BinarySearchTree/BST-Traversal/Inorder-Traversal/problem.js
+++ BinarySearchTree/BST-Traversal/Inorder-Traversal/problem.js
@@ -20,15 +20,7 @@ function Node(data) {
 */
 
 const inOrder = root => {
-    const array = [];
-    const recurse = root => {
-        if (root === null) return [];
-        if (root.left) recurse(root.left);
-        array.push(root.data);
-        if (root.right) recurse(root.right);
-    }
-    recurse(root);
-    return array;
+
 }
 
 module.exports = inOrder;
--- BinarySearchTree/BST-Traversal/Levelorder-Traversal/problem.js
+++ BinarySearchTree/BST-Traversal/Levelorder-Traversal/problem.js
@@ -20,7 +20,17 @@ function Node(data) {
 */
 
 const levelOrder = root => {
-
+    if (root === null) return [];
+    const array = [];
+    const queue = [root];
+    while (queue.length) {
+        const branch = queue.shift();
+        array.push(branch.data);
+        if (branch.left) queue.push(branch.left);
+        if (branch.right) queue.push(branch.right);
+    }
+    console.log(array);
+    return array;
 }
 
 module.exports = levelOrder;
