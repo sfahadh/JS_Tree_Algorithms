a1b37b8265f472329b0e4aac31c8d66840b961ec
Fahad <sfahad_hus@yahoo.com>
2020-08-19 10:55:40 -0400

    changed order of test cases in pre order traversal problem in BST

--- BinarySearchTree/BST-Traversal/Preorder-Traversal/problem.js
+++ BinarySearchTree/BST-Traversal/Preorder-Traversal/problem.js
@@ -20,7 +20,16 @@ function Node(data) {
 */
 
 const preOrder = root => {
-
+    const array = [];
+    const recurse = root => {
+        if (root === null) return [];
+        array.push(root.data);
+        if (root.left) recurse(root.left);
+        if (root.right) recurse(root.right);
+    }
+    recurse(root);
+    console.log(array);
+    return array;
 }
 
 module.exports = preOrder;
--- BinarySearchTree/BST-Traversal/Preorder-Traversal/problem.test.js
+++ BinarySearchTree/BST-Traversal/Preorder-Traversal/problem.test.js
@@ -1,10 +1,41 @@
 const constructTree = require("../../Starter/ConstructTree");
 const preOrder = require("./problem");
+const Structures = require("../../Starter/Structures");
 
-// Test Case 1: //* Sample Image: refer to Images -> BST-structure-2.png
+// Test Case 1: //* Sample Image: refer to Images -> BST-structure-1.png
+test("Return an array of values - [24, 17, 31, 34, 32, 39]", () => {
+    const tree = constructTree(Structures["1"]);
+    expect(preOrder(tree.root)).toEqual([24, 17, 31, 34, 32, 39]);
+});
+
+// Test Case 2: //* Sample Image: refer to Images -> BST-structure-2.png
 test("Return an array of values - [10, 6, 15]", () => {
-    const tree = constructTree([10, 6, 15]);
+    const tree = constructTree(Structures["2"]);
     expect(preOrder(tree.root)).toEqual([10, 6, 15]);
+});
+
+// Test Case 3: //* Sample Image: refer to Images -> BST-structure-3.png
+test("Return an array of values - [62, 123]", () => {
+    const tree = constructTree(Structures["3"]);
+    expect(preOrder(tree.root)).toEqual([62, 123]);
+});
+
+// Test Case 4: //* Sample Image: refer to Images -> BST-structure-4.png
+test("Return an array of values - [62, 13]", () => {
+    const tree = constructTree(Structures["4"]);
+    expect(preOrder(tree.root)).toEqual([62, 13]);
+});
+
+// Test Case 5: //* Sample Image: refer to Images -> BST-structure-5.png
+test("Return an array of values - [18, 5, 3, 0, 13, 65, 33]", () => {
+    const tree = constructTree(Structures["5"]);
+    expect(preOrder(tree.root)).toEqual([18, 5, 3, 0, 13, 65, 33]);
+});
+
+// Test Case 6: //* Sample Image: refer to Images -> BST-structure-6.png
+test("Return an array of values - [6, 4, 3, 5, 8, 7, 9]", () => {
+    const tree = constructTree(Structures["6"]);
+    expect(preOrder(tree.root)).toEqual([6, 4, 3, 5, 8, 7, 9]);
 });
 
 // Test Case 2:
@@ -13,37 +44,7 @@ test("Return an array of value - [33]", () => {
     expect(preOrder(tree.root)).toEqual([33]);
 });
 
-// Test Case 3: //* Sample Image: refer to Images -> BST-structure-1.png
-test("Return an array of values - [24, 17, 31, 34, 32, 39]", () => {
-    const tree = constructTree([24, 17, 31, 34, 32, 39]);
-    expect(preOrder(tree.root)).toEqual([24, 17, 31, 34, 32, 39]);
-});
-
 // Test Case 4:
 test("Return an empty array", () => {
     expect(preOrder(null)).toEqual([]);
-});
-
-// Test Case 5: //* Sample Image: refer to Images -> BST-structure-5.png
-test("Return an array of values - [18, 5, 3, 0, 13, 65, 33]", () => {
-    const tree = constructTree([18, 5, 65, 3, 13, 33, 0]);
-    expect(preOrder(tree.root)).toEqual([18, 5, 3, 0, 13, 65, 33]);
-});
-
-// Test Case 6: //* Sample Image: refer to Images -> BST-structure-3.png
-test("Return an array of values - [62, 123]", () => {
-    const tree = constructTree([62, 123]);
-    expect(preOrder(tree.root)).toEqual([62, 123]);
-});
-
-// Test Case 7: //* Sample Image: refer to Images -> BST-structure-4.png
-test("Return an array of values - [62, 13]", () => {
-    const tree = constructTree([62, 13]);
-    expect(preOrder(tree.root)).toEqual([62, 13]);
-});
-
-// Test Case 8: //* Sample Image: refer to Images -> BST-structure-6.png
-test("Return an array of values - [6, 4, 3, 5, 8, 7, 9]", () => {
-    const tree = constructTree([6, 4, 8, 3, 5, 7, 9]);
-    expect(preOrder(tree.root)).toEqual([6, 4, 3, 5, 8, 7, 9]);
 });
