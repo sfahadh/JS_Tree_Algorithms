51445e43ad8c6644764fe84521a936a7cd1bda46
Fahad <sfahad_hus@yahoo.com>
2020-08-20 16:44:24 -0400

    fixed test cases from replacement of BST strucutures for dual parents problem in BST

--- BinarySearchTree/Problems/DualParents/problem.test.js
+++ BinarySearchTree/Problems/DualParents/problem.test.js
@@ -28,7 +28,7 @@ test("There are 0 parents with a left and right child - Part II", () => {
 
 // Test Case 5: //* Sample Input: refer to Images -> BST-structure-5.png
 test("There are 2 parents, nodes 18 & 5 with a left and right child", () => {
-	const tree = constructTree(Strucutres["8"]);
+	const tree = constructTree(Strucutres["5"]);
 	expect(dualParent(tree.root)).toEqual(2);
 });
 
@@ -39,39 +39,39 @@ test("There are 3 parents, nodes 6, 4, & 8 with a left and right child", () => {
 });
 
 // Test Case 7: //* Sample Input: refer to Images -> BST-structure-7.png
-test("There are 3 parents, nodes 7, 5, & 13 with a left and right child", () => {
+test("There are 3 parents, nodes 33, 21, 40, 13, & 51 with a left and right child", () => {
 	const tree = constructTree(Strucutres["7"]);
-	expect(dualParent(tree.root)).toEqual(3);
+	expect(dualParent(tree.root)).toEqual(5);
 });
 
 // Test Case 8: //* Sample Input: refer to Images -> BST-structure-8.png
-test("There are 2 parents, nodes 25 & 16 with a left and right child", () => {
+test("There is only one parent, node 69 with a left and right child", () => {
 	const tree = constructTree(Strucutres["8"]);
-	expect(dualParent(tree.root)).toEqual(2);
-});
-
-// Test Case 9: //* Sample Input: refer to Images -> BST-structure-9.png
-test("There is only 1 parent, node 15 with a left and right child", () => {
-	const tree = constructTree(Strucutres["9"]);
 	expect(dualParent(tree.root)).toEqual(1);
 });
 
+// Test Case 9: //* Sample Input: refer to Images -> BST-structure-9.png
+test("There are 0 parents with a left and right child - Part II", () => {
+	const tree = constructTree(Strucutres["9"]);
+	expect(dualParent(tree.root)).toEqual(0);
+});
+
 // Test Case 10: //* Sample Input: refer to Images -> BST-structure-10.png
-test("There is 1 parent with a left and right child", () => {
+test("There is only 1 parent, node 15 with a left and right child", () => {
 	const tree = constructTree(Strucutres["10"]);
 	expect(dualParent(tree.root)).toEqual(1);
 });
 
 // Test Case 11: //* Sample Input: refer to Images -> BST-structure-11.png
-test("There are 3 parents with a left and right child", () => {
+test("There are 3 parents, nodes 25, 15, & 45 with a left and right child", () => {
 	const tree = constructTree(Strucutres["11"]);
 	expect(dualParent(tree.root)).toEqual(3);
 });
 
 // Test Case 12: //* Sample Input: refer to Images -> BST-structure-12.png
-test("There are 3 parents with a left and right child", () => {
+test("There are 2 parents, nodes 777 & 555 with a left and right child", () => {
 	const tree = constructTree(Strucutres["12"]);
-	expect(dualParent(tree.root)).toEqual(3);
+	expect(dualParent(tree.root)).toEqual(2);
 });
 
 // Test Case 13: //* Sample Input: refer to Images -> BST-structure-13.png
@@ -81,7 +81,7 @@ test("There are 6 parents, nodes 52, 25, 147, 6, 99, 2, & 123 with a left and ri
 });
 
 // Test Case 14:
-test("There are no parents with a left and right child", () => {
+test("There are no parents with a left and right child - Part III", () => {
 	const tree = constructTree([17]);
 	expect(dualParent(tree.root)).toEqual(0);
 });
