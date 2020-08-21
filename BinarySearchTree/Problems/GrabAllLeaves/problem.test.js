067be722f691fa3181632d803bf0f385c888bb27
Fahad <sfahad_hus@yahoo.com>
2020-08-21 13:40:35 -0400

    added 4 more test cases for grab all leaves problem in BST

--- BinarySearchTree/Problems/GrabAllLeaves/problem.test.js
+++ BinarySearchTree/Problems/GrabAllLeaves/problem.test.js
@@ -2,16 +2,34 @@ const constructTree = require("../../Starter/ConstructTree");
 const grabAllLeaves = require("./problem");
 const Structures = require("../../Starter/Structures");
 
+// Test Case 1: //* Sample Input: refer to Images -> BST-structure-1.png
+test("Return an array of [17, 32, 39] leaf nodes", () => {
+	const tree = constructTree(Structures["1"]);
+	expect(grabAllLeaves(tree.root)).toEqual([17, 32, 39]);
+});
+
 // Test Case 2: //* Sample Input: refer to Images -> BST-structure-2.png
 test("Return an array of [6, 15] leaf nodes", () => {
 	const tree = constructTree(Structures["2"]);
 	expect(grabAllLeaves(tree.root)).toEqual([6, 15]);
 });
 
+// Test Case 3: //* Sample Input: refer to Images -> BST-structure-3.png
+test("Return an array of [123] leaf nodes", () => {
+	const tree = constructTree(Structures["3"]);
+	expect(grabAllLeaves(tree.root)).toEqual([123]);
+});
+
 // Test Case 4: //* Sample Input: refer to Images -> BST-structure-4.png
 test("Return an array of [13] leaf nodes", () => {
 	const tree = constructTree(Structures["4"]);
 	expect(grabAllLeaves(tree.root)).toEqual([13]);
+});
+
+// Test Case 5: //* Sample Input: refer to Images -> BST-structure-5.png
+test("Return an array of [0, 13, 33] leaf nodes", () => {
+	const tree = constructTree(Structures["5"]);
+	expect(grabAllLeaves(tree.root)).toEqual([0, 13, 33]);
 });
 
 // Test Case 6: //* Sample Input: refer to Images -> BST-structure-6.png
@@ -30,6 +48,12 @@ test("Return an array of [12, 15, 23, 36, 49, 55] leaf nodes", () => {
 test("Return an array of [53, 85] leaf nodes", () => {
 	const tree = constructTree(Structures["8"]);
 	expect(grabAllLeaves(tree.root)).toEqual([53, 85]);
+});
+
+// Test Case 9: //* Sample Input: refer to Images -> BST-structure-9.png
+test("Return an array of [11] leaf nodes", () => {
+	const tree = constructTree(Structures["9"]);
+	expect(grabAllLeaves(tree.root)).toEqual([11]);
 });
 
 // Test Case 13: //* Sample Input: refer to Images -> BST-structure-13.png
