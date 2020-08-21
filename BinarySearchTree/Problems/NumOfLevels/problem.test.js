72619b06a48bd4c85dbc8797b25056d004911f76
Fahad <sfahad_hus@yahoo.com>
2020-08-21 14:23:50 -0400

    added 4 more test cases for num of levels problem in BST

--- BinarySearchTree/Problems/NumOfLevels/problem.test.js
+++ BinarySearchTree/Problems/NumOfLevels/problem.test.js
@@ -2,10 +2,34 @@ const constructTree = require("../../Starter/ConstructTree");
 const levels = require("./problem");
 const Structures = require("../../Starter/Structures");
 
+// Test Case 1: //* Sample Input: refer to Images -> BST-structure-1.png
+test("The BST structure 1 should return 4 as number of levels", () => {
+  const tree = constructTree(Structures["1"]);
+  expect(levels(tree.root)).toEqual(4);
+});
+
 // Test Case 2: //* Sample Input: refer to Images -> BST-structure-2.png
 test("The BST structure 2 should return 2 as number of levels", () => {
   const tree = constructTree(Structures["2"]);
   expect(levels(tree.root)).toEqual(2);
+});
+
+// Test Case 3: //* Sample Input: refer to Images -> BST-structure-3.png
+test("The BST structure 3 should return 2 as number of levels", () => {
+  const tree = constructTree(Structures["3"]);
+  expect(levels(tree.root)).toEqual(2);
+});
+
+// Test Case 4: //* Sample Input: refer to Images -> BST-structure-4.png
+test("The BST structure 4 should return 2 as number of levels", () => {
+  const tree = constructTree(Structures["4"]);
+  expect(levels(tree.root)).toEqual(2);
+});
+
+// Test Case 5: //* Sample Input: refer to Images -> BST-structure-5.png
+test("The BST structure 5 should return 4 as number of levels", () => {
+  const tree = constructTree(Structures["5"]);
+  expect(levels(tree.root)).toEqual(4);
 });
 
 // Test Case 6: //* Sample Input: refer to Images -> BST-structure-6.png
