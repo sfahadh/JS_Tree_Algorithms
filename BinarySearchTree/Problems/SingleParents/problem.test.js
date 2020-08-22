ee0faf9cd1fc49d9c9cace140467508d99ea6d1d
Fahad <sfahad_hus@yahoo.com>
2020-08-22 15:02:34 -0400

    added 4 more test cases for single parents problem in BST

--- BinarySearchTree/Problems/SingleParents/problem.test.js
+++ BinarySearchTree/Problems/SingleParents/problem.test.js
@@ -2,14 +2,38 @@ const constructTree = require("../../Starter/ConstructTree");
 const singleParent = require("./problem");
 const Structures = require("../../Starter/Structures");
 
+// Test Case 1: //* Sample Input: refer to Images -> BST-structure-1.png
+test("There is only 1 single parent, node 31", () => {
+	const tree = constructTree(Structures["1"]);
+	expect(singleParent(tree.root)).toEqual(1);
+});
+
 // Test Case 2: //* Sample Input: refer to Images -> BST-structure-2.png
-test("There are 0 nodes that are single parents", () => {
+test("There are 0 nodes that are single parents - Part I", () => {
 	const tree = constructTree(Structures["2"]);
 	expect(singleParent(tree.root)).toEqual(0);
 });
 
+// Test Case 3: //* Sample Input: refer to Images -> BST-structure-3.png
+test("There is only 1 single parent, node 62 - Part I", () => {
+	const tree = constructTree(Structures["3"]);
+	expect(singleParent(tree.root)).toEqual(1);
+});
+
+// Test Case 4: //* Sample Input: refer to Images -> BST-structure-4.png
+test("There is only 1 single parent, node 62 - Part II", () => {
+	const tree = constructTree(Structures["4"]);
+	expect(singleParent(tree.root)).toEqual(1);
+});
+
+// Test Case 5: //* Sample Input: refer to Images -> BST-structure-5.png
+test("There are 2 single parent, nodes 65 & 3", () => {
+	const tree = constructTree(Structures["5"]);
+	expect(singleParent(tree.root)).toEqual(2);
+});
+
 // Test Case 6: //* Sample Input: refer to Images -> BST-structure-6.png
-test("There are 0 nodes that are single parents", () => {
+test("There are 0 nodes that are single parents - Part II", () => {
 	const tree = constructTree(Structures["6"]);
 	expect(singleParent(tree.root)).toEqual(0);
 });
