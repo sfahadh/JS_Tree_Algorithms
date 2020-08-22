ec5d5f6260c8a15e63eb5e4f4f44e9072eeb8119
Fahad <sfahad_hus@yahoo.com>
2020-08-22 12:22:24 -0400

    changed order of test cases in same parent problem in BST

--- BinarySearchTree/Problems/SameParent/problem.test.js
+++ BinarySearchTree/Problems/SameParent/problem.test.js
@@ -1,43 +1,45 @@
 const constructTree = require("../../Starter/ConstructTree");
 const sameParent = require("./problem");
-
-// Test Case 1: //* Sample Input: refer to Images -> BST-structure-7.png
-test("Return true because 5 and 13 does have the same parent", () => {
-	const tree = constructTree([7, 5, 13, 2, 6, 9, 15, 1]);
-	expect(sameParent(tree.root, 5, 13)).toEqual(true);
-});
+const Structures = require("../../Starter/Structures");
 
 // Test Case 2: //* Sample Input: refer to Images -> BST-structure-2.png
 test("Return false because 25 and 43 does not have the same parent", () => {
-	const tree = constructTree([10, 6, 15]);
+	const tree = constructTree(Structures["2"]);
 	expect(sameParent(tree.root, 6, 43)).toEqual(false);
 });
 
-// Test Case 3:
+// Test Case 6: //* Sample Input: refer to Images -> BST-structure-6.png
+test("Return false because 8 and 16 does not have the same parent", () => {
+	const tree = constructTree(Structures["6"]);
+	expect(sameParent(tree.root, 5, 7)).toEqual(false);
+});
+
+// Test Case 7: //* Sample Input: refer to Images -> BST-structure-7.png
+test("Return true because 5 and 13 does have the same parent", () => {
+	const tree = constructTree(Structures["7"]);
+	expect(sameParent(tree.root, 5, 13)).toEqual(true);
+});
+
+// Test Case 10: //* Sample Input: refer to Images -> BST-structure-10.png
+test("Return false because 3 and 17 does not have the same parent", () => {
+	const tree = constructTree(Structures["10"]);
+	expect(sameParent(tree.root, 12, 19)).toEqual(true);
+});
+
+// Test Case 11: //* Sample Input: refer to Images -> BST-structure-11.png
+test("Return true because 5 and 21 does have the same parent", () => {
+	const tree = constructTree(Structures["11"]);
+	expect(sameParent(tree.root, 42, 50)).toEqual(true);
+});
+
+// Test Case 14:
 test("Return false because there is only 1 node", () => {
 	const tree = constructTree([17]);
 	expect(sameParent(tree.root, 14, 32)).toEqual(false);
 });
 
-// Test Case 4: //* Sample Input: refer to Images -> BST-structure-6.png
-test("Return false because 8 and 16 does not have the same parent", () => {
-	const tree = constructTree([6, 4, 8, 3, 5, 7, 9]);
-	expect(sameParent(tree.root, 5, 7)).toEqual(false);
-});
-
-// Test Case 5:
+// Test Case 15:
 test("Return false because there is no tree", () => {
 	expect(sameParent(null)).toEqual(false);
 });
 
-// Test Case 6: //* Sample Input: refer to Images -> BST-structure-11.png
-test("Return true because 5 and 21 does have the same parent", () => {
-	const tree = constructTree([25, 15, 30, 5, 20, 35, 45, 42, 50, 69]);
-	expect(sameParent(tree.root, 42, 50)).toEqual(true);
-});
-
-// Test Case 7: //* Sample Input: refer to Images -> BST-structure-10.png
-test("Return false because 3 and 17 does not have the same parent", () => {
-	const tree = constructTree([15, 12, 19, 2, 21]);
-	expect(sameParent(tree.root, 12, 19)).toEqual(true);
-});
