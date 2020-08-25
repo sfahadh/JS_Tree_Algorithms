bbf47c07ac381e8c6ed4b5c075ca08efbf41d507
Fahad <sfahad_hus@yahoo.com>
2020-08-25 10:41:28 -0400

    created structures js file for 10th N-ary tree

---
+++ N-aryTree/Starter/Structures/N-aryTree10.js
@@ -0,0 +1,21 @@
+const NaryTree = require("../N-aryTree");
+const InsertToNode = require("../insertToNode");
+
+const constructTree = () => {
+    const tree = NaryTree(51);
+    InsertToNode(tree, [25, 50, 75, 100]);
+
+    InsertToNode(tree.children[1], [40, 30]);
+    InsertToNode(tree.children[2], [55]);
+    InsertToNode(tree.children[3], [99, 66]);
+
+    InsertToNode(tree.children[1].children[1], [20]);
+    InsertToNode(tree.children[2].children[0], [35]);
+    InsertToNode(tree.children[3].children[0], [3]);
+
+    InsertToNode(tree.children[1].children[1].children[0], [10]);
+
+    return tree;
+}
+
+module.exports = constructTree;
