feda3272e8a8527e27a5466e2942b0a815c9c8f7
Fahad <sfahad_hus@yahoo.com>
2020-08-25 16:02:00 -0400

    cleared out the test cases for level order traversal problem in N-ary tree

--- N-aryTree/Traversal/LevelOrder/problem.js
+++ N-aryTree/Traversal/LevelOrder/problem.js
@@ -3,14 +3,16 @@
 /* // Uncomment the imports below if you plan on using them
  * const NaryTree = require("../../Starter/N-aryTree");
  * const Node = require("../../Starter/Node");
- */
+*/
 
 /*
 TODO: Complete the following level-order N-ary Tree traversal problem below. There is only 1 input, which is the root of the tree. The level-order traversal for N-ary trees is defined as traversing down the tree, starting from the root, which is the first row or level of the tree, visit that node and then go down a level, grabbing all the child nodes starting from the left and moving on towards the right. Then repeat that step by going down to the next level until you reach the last. All nodes visited should be pushed into an array and then returned as a result.
 
-function NaryTree() {
-    this.root = null;
+function NaryTree(data = null) {
+    this.root = data === null ? null : new Node(data);
+    return this.root;
 }
+
 
 function Node(data) {
     this.data = data;
@@ -25,19 +27,11 @@ const levelOrder = root => {
 module.exports = levelOrder;
 
 /*
-! Test Case 1: 
-* Sample Input: 
-        5
-    /   |   \
-  13    3    23
-* Sample Output: [13, 3, 23, 5] 
+! Test Case 1:
+* Sample Input: //* Sample Image: refer to Images -> Nary-structure-3.png
+* Sample Output: [0, 1, 2, 3]
 
 ! Test Case 2:
-* Sample Input: 
-            5
-        /   |   \
-      13    3    23
-    /  |           \  
-   7   24           63 
-* Sample Output: [7, 24, 63, 13, 3, 23, 5] 
+* Sample Input: //* Sample Image: refer to Images -> Nary-structure-6.png
+* Sample Output: [21, 13, 7, 7, 12, 2, 71, 31, 53, 6, 22]
 */
