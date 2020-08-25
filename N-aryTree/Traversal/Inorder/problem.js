86cf7550b5e9ca6343c797d7d777a3cca8e63132
Fahad <sfahad_hus@yahoo.com>
2020-08-25 11:48:18 -0400

    cleared out test cases for in order traversal problem in N-ary tree

--- N-aryTree/Traversal/Inorder/problem.js
+++ N-aryTree/Traversal/Inorder/problem.js
@@ -8,8 +8,9 @@
 /*
 TODO: Complete the following in-order N-ary Tree traversal problem below. There is only 1 input, which is the root of the tree. The in-order traversal for N-ary trees is defined as traversing down the tree, visiting all the children nodes except for the last of the children nodes, then visiting the parent node, and then back down to the last of the children nodes. All nodes visited should be pushed into an array and then returned as a result.
 
-function NaryTree() {
-    this.root = null;
+function NaryTree(data = null) {
+    this.root = data === null ? null : new Node(data);
+    return this.root;
 }
 
 function Node(data) {
@@ -25,19 +26,11 @@ const inOrder = root => {
 module.exports = inOrder;
 
 /*
-! Test Case 1: 
-* Sample Input: 
-        5
-    /   |   \
-   13   3    23
-* Sample Output: [13, 3, 5, 23] 
+! Test Case 1:
+* Sample Input: //* Sample Image: refer to Images -> Nary-structure-3.png
+* Sample Output: [1, 2, 0, 3]
 
 ! Test Case 2:
-* Sample Input: 
-            5
-        /   |   \
-      13    3    23
-    /  |           \  
-   7   24           63 
-* Sample Output: [7, 13, 24, 3, 5, 23, 63] 
+* Sample Input: //* Sample Image: refer to Images -> Nary-structure-6.png
+* Sample Output: [31, 7, 53, 13, 12, 21, 2, 7, 6, 71, 22]
 */
