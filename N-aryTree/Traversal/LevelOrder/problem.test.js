e0e9781b7b5d8b35f3c6561465f3e5b22beca6ac
Fahad <sfahad_hus@yahoo.com>
2020-08-25 16:07:39 -0400

    added 3 test cases to in level traversal problem in N-ary tree

--- N-aryTree/Traversal/LevelOrder/problem.test.js
+++ N-aryTree/Traversal/LevelOrder/problem.test.js
@@ -1,6 +1,26 @@
 const levelOrder = require("./problem");
+const NaryTree1 = require("../../Starter/Structures/N-aryTree1");
+const NaryTree2 = require("../../Starter/Structures/N-aryTree2");
+const NaryTree3 = require("../../Starter/Structures/N-aryTree3");
+const NaryTree4 = require("../../Starter/Structures/N-aryTree4");
+const NaryTree5 = require("../../Starter/Structures/N-aryTree5");
+const NaryTree6 = require("../../Starter/Structures/N-aryTree6");
+const NaryTree7 = require("../../Starter/Structures/N-aryTree7");
+const NaryTree8 = require("../../Starter/Structures/N-aryTree8");
+const NaryTree9 = require("../../Starter/Structures/N-aryTree9");
+const NaryTree10 = require("../../Starter/Structures/N-aryTree10");
 
-// Test Case 1:
-test("Return an array of values - ", () => {
-    expect(levelOrder()).toEqual();
+// Test Case 1: //* Sample Image: refer to Images -> Nary-structure-1.png
+test("Return an array of values - [14, 51]", () => {
+    expect(levelOrder(NaryTree1())).toEqual([14, 51]);
+});
+
+// Test Case 2: //* Sample Image: refer to Images -> Nary-structure-2.png
+test("Return an array of values - [96, 6, 9]", () => {
+    expect(levelOrder(NaryTree2())).toEqual([96, 6, 9]);
+});
+
+// Test Case 3: //* Sample Image: refer to Images -> Nary-structure-3.png
+test("Return an array of values - [0, 1, 2, 3]", () => {
+    expect(levelOrder(NaryTree3())).toEqual([0, 1, 2, 3]);
 });
