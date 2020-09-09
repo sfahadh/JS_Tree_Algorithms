dd50c50cda1aa1905461cfd3574e6df921719024
sfahadh <sfahadh14@gmail.com>
2020-09-09 10:25:42 -0400

    added BinaryTree insert function in Binary Tree

--- BinaryTree/Starter/BinaryTree.js
+++ BinaryTree/Starter/BinaryTree.js
@@ -4,5 +4,23 @@ function BinaryTree() {
     this.root = null;
 }
 
+BinaryTree.prototype.insert = function (parent, data, insertLeft) {
+    const newNode = new Node(data);
+    if (!this.root) return this.root = newNode;
+    const queue = [this.root];
+
+    while (queue.length) {
+        const node = queue.shift();
+
+        if (node.data === parent) {
+            insertLeft ? node.left = newNode : node.right = newNode;
+            break;
+        } else {
+            queue.push(node.left);
+            queue.push(node.right);
+        }
+    }
+}
+
 
 module.exports = BinaryTree;
