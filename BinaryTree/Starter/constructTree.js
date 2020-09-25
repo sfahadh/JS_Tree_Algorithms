02ed79b0433485a54cd098a9d91e332ac7c7a92c
sfahadh <sfahadh14@gmail.com>
2020-09-25 14:48:06 -0400

    added construct tree file in Binary Tree

---
+++ BinaryTree/Starter/constructTree.js
@@ -0,0 +1,14 @@
+const Node = require("./Node");
+
+const constructTree = arr => {
+    let data = arr.shift();
+    if (!data) return null;
+    const node = new Node(data);
+
+    node.left = constructTree(arr);
+    node.right = constructTree(arr);
+
+    return node;
+}
+
+module.exports = constructTree;
