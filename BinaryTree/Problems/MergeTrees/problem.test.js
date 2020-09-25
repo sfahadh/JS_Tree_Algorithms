0b427b321f30e94803f3565aa40afb59ac5d45b7
sfahadh <sfahadh14@gmail.com>
2020-09-25 15:17:37 -0400

    added 2 more test cases for merge trees problem in Binary Tree

--- BinaryTree/Problems/MergeTrees/problem.test.js
+++ BinaryTree/Problems/MergeTrees/problem.test.js
@@ -11,3 +11,21 @@ test("Merge two tree to form Binary Structure 4", () => {
     tree2.root = constructTree([1, 1]);
     expect(mergeTrees(tree1.root, tree2.root)).toEqual(constructTree(structures["1"]));
 });
+
+// Test Case 2: //* Sample Output: refer to Images -> BT-structure-2.png
+test("Merge two tree to form Binary Structure 4", () => {
+    const tree1 = new BinaryTree();
+    const tree2 = new BinaryTree();
+    tree1.root = constructTree([3]);
+    tree2.root = constructTree([11, null, 18]);
+    expect(mergeTrees(tree1.root, tree2.root)).toEqual(constructTree(structures["2"]));
+});
+
+// Test Case 3: //* Sample Output: refer to Images -> BT-structure-3.png
+test("Merge two tree to form Binary Structure 4", () => {
+    const tree1 = new BinaryTree();
+    const tree2 = new BinaryTree();
+    tree1.root = constructTree([17, 22, null, null, 11]);
+    tree2.root = constructTree([0, 0, null, null, 0]);
+    expect(mergeTrees(tree1.root, tree2.root)).toEqual(constructTree(structures["3"]));
+});
