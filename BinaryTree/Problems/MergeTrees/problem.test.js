0762ca9b077af7db58b114e39fa776c4c5310288
sfahadh <sfahadh14@gmail.com>
2020-09-25 14:53:26 -0400

    added structures file in Binary Tree

--- BinaryTree/Problems/MergeTrees/problem.test.js
+++ BinaryTree/Problems/MergeTrees/problem.test.js
@@ -5,6 +5,8 @@ const BinaryTree4 = require("../../Starter/Structures/BinaryTree4");
 
 // Test Case 4: //* Sample Output: refer to Images -> BT-structure-4.png
 test("Merge two tree to form Binary Structure 4", () => {
+    const tree1 = new BinaryTree;
+    tree1.root = new Node(3);
 
     expect(mergeTrees()).toEqual(BinaryTree4);
 });
---
+++ BinaryTree/Starter/Structures.js
@@ -0,0 +1,7 @@
+const structures = {
+    1: [10, 3],
+    2: [14, null, 18],
+    3: [17, 22, null, null, 11],
+}
+
+module.exports = structures;
