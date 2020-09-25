1eb53dd60edbb104877e5c962bdcf81b6e6e495c
sfahadh <sfahadh14@gmail.com>
2020-09-25 14:49:03 -0400

    deleted binary tree insert function in Binary Tree

--- BinaryTree/Starter/BinaryTree.js
+++ BinaryTree/Starter/BinaryTree.js
@@ -1,25 +1,5 @@
-const Node = require("./Node");
-
 function BinaryTree() {
     this.root = null;
-}
-
-BinaryTree.prototype.insert = function (data, parent, insertLeft) {
-    const newNode = new Node(data);
-    if (!this.root) return this.root = newNode;
-    const queue = [this.root];
-
-    while (queue.length) {
-        const node = queue.shift();
-
-        if (node.data !== parent || node.left && node.right) {
-            queue.push(node.left);
-            queue.push(node.right);
-        } else {
-            insertLeft ? node.left = newNode : node.right = newNode;
-            break;
-        }
-    }
 }
 
 module.exports = BinaryTree;
