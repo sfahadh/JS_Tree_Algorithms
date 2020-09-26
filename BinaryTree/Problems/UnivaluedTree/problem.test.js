34ae0cc248cbed857b27a99201b29ee6a37f62d1
sfahadh <sfahadh14@gmail.com>
2020-09-26 13:18:44 -0400

    added 2nd test case and added 11th Binary tree structure in univalued tree problem

--- BinaryTree/Problems/UnivaluedTree/problem.test.js
+++ BinaryTree/Problems/UnivaluedTree/problem.test.js
@@ -7,3 +7,8 @@ const isUnivalued = require("./problem");
 test("Return true because all node values have the value 4", () => {
     expect(isUnivalued(constructTree(structures["10"]))).toEqual(true);
 });
+
+// Test Case 2: //* Sample Output: refer to Images -> BT-structure-11.png
+test("Return true because all node values have the value 12", () => {
+    expect(isUnivalued(constructTree(structures["11"]))).toEqual(true);
+});
