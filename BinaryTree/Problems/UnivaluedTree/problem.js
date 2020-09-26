9115130787f9c0aba7e49ae93ebc4a7f1301daff
sfahadh <sfahadh14@gmail.com>
2020-09-26 12:31:13 -0400

    added univalued tree problem in Binary Tree

---
+++ BinaryTree/Problems/UnivaluedTree/problem.js
@@ -0,0 +1,38 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BinaryTree = require("../../Starter/BinaryTree");
+ * const Node = require("../../Starter/Node");
+ */
+
+/*
+TODO: Complete the following univalued tree problem below. There is only 1 input, which is the root
+TODO: of the Binary Tree. Univalued means that all the nodes of the Binary Tree has the same value.
+TODO: If there are 1 or more nodes different from the others, return false, otherwise if all nodes
+TODO: have the same value, return true.
+
+function BinaryTree() {
+    this.root = null;
+}
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const isUnivalued = root => {
+
+};
+
+module.exports = isUnivalued;
+
+/*
+! Test Case:
+* Sample Input:
+
+* Sample Output:
+*/
+
+// ? If the average ends up being a decimal, floor the value;
