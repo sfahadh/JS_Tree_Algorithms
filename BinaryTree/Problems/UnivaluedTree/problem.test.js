61a6eb0a551c3d1748a522657c2bed2d85b7ab33
sfahadh <sfahadh14@gmail.com>
2020-09-26 13:33:17 -0400

    changed order of binary tree structure images 10 - 16

--- BinaryTree/Problems/UnivaluedTree/problem.test.js
+++ BinaryTree/Problems/UnivaluedTree/problem.test.js
@@ -3,12 +3,12 @@ const structures = require("../../Starter/Structures");
 const constructTree = require("../../Starter/constructTree");
 const isUnivalued = require("./problem");
 
-// Test Case 1: //* Sample Output: refer to Images -> BT-structure-10.png
+// Test Case 1: //* Sample Output: refer to Images -> BT-structure-11.png
 test("Return true because all node values have the value 4", () => {
-    expect(isUnivalued(constructTree(structures["10"]))).toEqual(true);
+    expect(isUnivalued(constructTree(structures["11"]))).toEqual(true);
 });
 
-// Test Case 2: //* Sample Output: refer to Images -> BT-structure-11.png
+// Test Case 2: //* Sample Output: refer to Images -> BT-structure-12.png
 test("Return true because all node values have the value 12", () => {
-    expect(isUnivalued(constructTree(structures["11"]))).toEqual(true);
+    expect(isUnivalued(constructTree(structures["12"]))).toEqual(true);
 });
