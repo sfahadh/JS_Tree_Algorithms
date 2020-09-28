3bf59286e4b491b64130865166a4d866d32c9f72
sfahadh <sfahadh14@gmail.com>
2020-09-28 12:47:09 -0400

    added 4 more test cases for symmetric problem in binary tree

--- BinaryTree/Problems/Symmetric/problem.test.js
+++ BinaryTree/Problems/Symmetric/problem.test.js
@@ -7,3 +7,23 @@ const symmetric = require("./problem");
 test("Return false because there is no right subtree, only a left subtree", () => {
     expect(symmetric(constructTree(structures["1"]))).toEqual(false);
 });
+
+// Test Case 2: //* Sample Output: refer to Images -> BT-structure-11.png
+test("Return false because there isn't an equal number of nodes in the right subtree and left subtree", () => {
+    expect(symmetric(constructTree(structures["11"]))).toEqual(false);
+});
+
+// Test Case 3: //* Sample Output: refer to Images -> BT-structure-16.png
+test("Return false because tree is not symmetric - Part I", () => {
+    expect(symmetric(constructTree(structures["16"]))).toEqual(false);
+});
+
+// Test Case 4: //* Sample Output: refer to Images -> BT-structure-16.png
+test("Return false because tree is symmetric - Part I", () => {
+    expect(symmetric(constructTree(structures["16"]))).toEqual(true);
+});
+
+// Test Case 5: //* Sample Output: refer to Images -> BT-structure-5.png
+test("Return false because one of the node has a different value", () => {
+    expect(symmetric(constructTree(structures["5"]))).toEqual(false);
+});
