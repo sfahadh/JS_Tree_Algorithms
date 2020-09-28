769d4f18217ed33a66d7c38d822274a9e7446aa0
sfahadh <sfahadh14@gmail.com>
2020-09-28 10:56:26 -0400

    added symmetric tree problem in binary tree

--- BinaryTree/Problems/Subtree/problem.js
+++ BinaryTree/Problems/Subtree/problem.js
@@ -22,15 +22,8 @@ function Node(data) {
 */
 
 const subtree = (root, sub) => {
-    if (!root || !sub) return false;
-    return compareTree(root, sub) || subtree(root.left, sub) || subtree(root.right, sub);
-};
 
-const compareTree = (root, sub) => {
-    if (!root && !sub) return true;
-    if ((!root && sub) || (root && !sub) || root.data !== sub.data) return false;
-    return compareTree(root.left, sub.left) && compareTree(root.right, sub.right);
-}
+};
 
 module.exports = subtree;
 
---
+++ BinaryTree/Problems/Symmetric/problem.js
@@ -0,0 +1,36 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BinaryTree = require("../../Starter/BinaryTree");
+ * const Node = require("../../Starter/Node");
+ */
+
+/*
+TODO: Complete the following symmetric tree problem below. There is only 1 input, which is the root
+TODO: of the Binary Tree. The goal of this problem is to check if the left branches equals to the 
+TODO: right branches of each node of the tree.
+
+function BinaryTree() {
+    this.root = null;
+}
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const symmetric = root => {
+
+};
+
+module.exports = symmetric;
+
+/*
+! Test Case:
+* Sample Input: refer to Images -> BT-structure-4.png
+* Sample Output: true
+*/
+
+// ? If the average ends up being a decimal, floor the value;
