8b521f363eb6ecd1e323ee07d6daaf33ac8c86b0
sfahadh <sfahadh14@gmail.com>
2020-09-27 23:32:03 -0400

    added subtree problem in Binary Tree

---
+++ BinaryTree/Problems/Subtree/problem.js
@@ -0,0 +1,50 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BinaryTree = require("../../Starter/BinaryTree");
+ * const Node = require("../../Starter/Node");
+*/
+
+/*
+TODO: Complete the following subtree of another tree problem below. There are 2 inputs, first input is the 
+TODO: root of a binary tree and the 2nd input is the root of the possible subtree. The goal of this problem
+TODO: is to check if the 2nd input is a subtree of the first input.
+
+function BinaryTree() {
+    this.root = null;
+}
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const subtree = (root, sub) => {
+
+};
+
+module.exports = subtree;
+
+/*
+!Test Case 1:
+* Sample Input:
+        1               3
+      /   \           /   \
+     3     2         7     6
+    / \             /
+   7   6           8
+* Sample Output: false
+
+! Test Case 2:
+* Sample Input:
+        1
+      /   \            3
+     3     2         /   \
+    / \             7     6
+   7   6
+* Sample Output: true
+*/
+
+// ? If the average ends up being a decimal, floor the value;
