5bf4503717a35d76d22635f7ec8df6153bd704a4
sfahadh <sfahadh14@gmail.com>
2020-09-28 13:30:34 -0400

    added balanced tree problem in binary tree

---
+++ BinaryTree/Problems/BalancedTree/problem.js
@@ -0,0 +1,37 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BinaryTree = require("../../Starter/BinaryTree");
+ * const Node = require("../../Starter/Node");
+ */
+
+/*
+TODO: Complete the following balanced tree problem below. There is only 1 input, which is the root
+TODO: of the Binary Tree. The goal of this problem is to check if the tree is balanced. A tree is 
+TODO: balanced when the difference between the left and right subtrees of every node is less than
+TODO: or equal to 1.
+
+function BinaryTree() {
+    this.root = null;
+}
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const balanced = root => {
+
+}
+
+module.exports = balanced;
+
+/*
+! Test Case:
+* Sample Input: refer to Images -> BT-structure-8.png
+* Sample Output: true
+*/
+
+// ? If the average ends up being a decimal, floor the value;
