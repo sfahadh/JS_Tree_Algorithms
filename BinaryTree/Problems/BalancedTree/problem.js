89f31f0be0bbcc00c5bf77ae560ec44ec9e5e9b5
sfahadh <sfahadh14@gmail.com>
2020-09-28 13:32:25 -0400

    added solution file for balanced tree problem in binary tree

--- BinaryTree/Problems/BalancedTree/problem.js
+++ BinaryTree/Problems/BalancedTree/problem.js
@@ -23,7 +23,18 @@ function Node(data) {
 */
 
 const balanced = root => {
+    let isbalanced = true;
 
+    (function recurse(root) {
+        if (!root) return -1;
+        const left = recurse(root.left);
+        const right = recurse(root.right);
+
+        if (Math.abs(left - right) > 1) isbalanced = false;
+        return Math.max(left, right) + 1;
+    })(root)
+
+    return isbalanced;
 }
 
 module.exports = balanced;
