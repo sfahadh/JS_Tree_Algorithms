955dc90e9b82ee01b27e0b7a35ca93a4b215cf60
sfahadh <sfahadh14@gmail.com>
2020-09-27 22:17:45 -0400

    added 4 more test cases for same tree problem

--- BinaryTree/Problems/SameTree/problem.test.js
+++ BinaryTree/Problems/SameTree/problem.test.js
@@ -8,3 +8,26 @@ test("Trees are identical - Part I", () => {
     const tree = constructTree(structures["1"]);
     expect(sameTree(tree, tree)).toEqual(true);
 });
+
+// Test Case 2: //* Sample Output: refer to Images -> BT-structure-2.png
+test("Return false because only one tree is an empty tree", () => {
+    expect(sameTree(constructTree(structures["2"]), new BinaryTree().root)).toEqual(false);
+});
+
+// Test Case 3: //* Sample Output: refer to Images -> BT-structure-3.png
+test("Return false because the left subtree is switched with the right subtree under node 17", () => {
+    expect(sameTree(constructTree(structures["3"]), new BinaryTree().root)).toEqual(false);
+});
+
+// Test Case 4: //* Sample Output: refer to Images -> BT-structure-4.png
+test("Return false because the left subtree is switched with the right subtree under node 12 in right subtree", () => {
+    const tree = constructTree([6, 12, 4, null, null, 3, 12, 4, null, null, 3])
+    expect(sameTree(constructTree(structures["4"]), tree)).toEqual(false);
+});
+
+// Test Case 5: //* Sample Output: refer to Images -> BT-structure-5.png
+test("Return false because the value of the root node is different", () => {
+    const tree = constructTree([23, 12, 11, null, null, null, 21, null, 11])
+    expect(sameTree(constructTree(structures["5"]), tree)).toEqual(false);
+});
+
