608570e6c5ac960c14a4b0fb23501b76f67a0dc4
sfahadh <sfahadh14@gmail.com>
2020-09-28 13:11:10 -0400

    added 17th structure in structure file in binary tree

--- BinaryTree/Starter/Structures.js
+++ BinaryTree/Starter/Structures.js
@@ -15,6 +15,7 @@ const structures = {
     14: [5, 5, 5, 5, null, null, null, 5, 2, null, null, 5, 5, 5, 5, null, null, null, 5, 5, null, null, 3],
     15: [8, 8, 8, null, null, 6, 8, null, null, 8, null, null, 8, 8, null, null, 8],
     16: [1, 1, 1, null, null, 1, 1, null, null, 1, null, null, 1, 1, null, null, 1, 1, null, null, 1],
+    17: [1, 2, 4, null, null, 5, null, null, 3, 6, null, null, 7],
     18: [9, 2, 7, null, null, 6, 6, null, null, null, 2, 6, null, 6, null, null, 7],
     19: [32, -3, 6, -1, null, null, null, 8, -12, -6, null, null, 9, null, null, null, -3, 8, null, -12, 9, null, null, -6, null, null, 6, null, -1],
     20: [96, 19, -5, 39, null, null, 51, null, null, -21, -3, null, null, 15, null, null, 19, -21, 15, null, null, -3, null, null, -5, 51, null, null, 39]
