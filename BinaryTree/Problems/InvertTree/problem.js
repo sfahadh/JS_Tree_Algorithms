9266e456415947ec57b75213840f5db38741b705
sfahadh <sfahadh14@gmail.com>
2020-09-27 20:56:52 -0400

    added same tree problem

--- BinaryTree/Problems/InvertTree/problem.js
+++ BinaryTree/Problems/InvertTree/problem.js
@@ -21,13 +21,7 @@ function Node(data) {
 */
 
 const invertTree = root => {
-    if (!root) return null;
 
-    let temp = root.left;
-    root.left = invertTree(root.right);
-    root.right = invertTree(temp);
-
-    return root;
 };
 
 module.exports = invertTree;
---
+++ BinaryTree/Problems/SameTree/problem.js
@@ -0,0 +1,37 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+ * const BinaryTree = require("../../Starter/BinaryTree");
+ * const Node = require("../../Starter/Node");
+*/
+
+/*
+TODO: Complete the following same tree problem below. There are 2 inputs, which are the roots of 2
+TODO: separate binary trees. The goal of the problem is to determine if the 2 trees are exactly 
+TODO: identical. Return true if they are, otherwise return false.
+
+function BinaryTree() {
+    this.root = null;
+}
+
+function Node(data) {
+    this.data = data;
+    this.left = null; 
+    this.right = null;
+}
+*/
+
+const sameTree = (root1, root2) => {
+
+};
+
+module.exports = sameTree;
+
+/*
+! Test Case:
+* Sample Input:
+     1          1
+   /   \      /   \
+  3     2    2     3
+* Sample Output: false
+*/
