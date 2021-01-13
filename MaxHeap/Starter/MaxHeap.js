a20af2a0e34aa6fe08999ffdf2718b8ccd5e77ce
sfahadh <sfahadh14@gmail.com>
2021-01-13 15:14:16 -0500

    added MinHeap folder

--- MaxHeap/Starter/MaxHeap.js
+++ MaxHeap/Starter/MaxHeap.js
@@ -24,4 +24,10 @@ MaxHeap.prototype.bubbleUp = function () {
     }
 }
 
+const heap = new MaxHeap();
+[48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41].forEach(node => {
+    heap.insert(node);
+})
+console.log(heap);
+
 module.exports = MaxHeap;
