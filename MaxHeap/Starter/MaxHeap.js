0a7076d541d09c9fd0e9643be18130ce9747e0e7
sfahadh <sfahadh14@gmail.com>
2021-01-13 14:43:17 -0500

    removed insert file in MaxHeap and moved the code to MaxHeap starter file

--- MaxHeap/Starter/MaxHeap.js
+++ MaxHeap/Starter/MaxHeap.js
@@ -2,4 +2,26 @@ function MaxHeap() {
     this.values = [];
 }
 
+MaxHeap.prototype.insert = function (data) {
+    this.values.push(data);
+    this.bubbleUp();
+}
+
+MaxHeap.prototype.bubbleUp = function () {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    let parentIndex;
+    let parent;
+
+    while (index > 0) {
+        parentIndex = Math.floor((index - 1) / 2);
+        parent = this.values[parentIndex];
+        if (element <= parent) break;
+
+        this.values[parentIndex] = element;
+        this.values[index] = parent;
+        index = parentIndex;
+    }
+}
+
 module.exports = MaxHeap;
